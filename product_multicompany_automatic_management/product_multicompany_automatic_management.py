3725c86363e24c09e7fbe71ee314ff1ddefc1c44
Jonatan Cloutier <jonatan.cloutier@savoirfairelinux.com>
2013-04-25 18:25:12 -0400

    add automatique multicompany management in product

---
+++ product_multicompany_automatic_management/product_multicompany_automatic_management.py
@@ -0,0 +1,34 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#    
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2010 Savoir-faire Linux (<http://www.savoirfairelinux.com>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.     
+#
+##############################################################################
+
+from openerp.osv import fields, orm
+
+class product_template(orm.Model):
+    _inherit = 'product.template'
+    
+    _columns = {
+        'company_id': fields.many2one('res.company', 'Company', required=True),
+    }
+    
+    _defaults = {
+        'company_id': lambda self,cr,uid,ctx: self.pool['res.company']._company_default_get(cr,uid,object='product.template',context=ctx)
+    }
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
