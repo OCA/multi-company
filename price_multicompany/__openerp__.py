dc7e9fa093a9b101dd98bb748e6b389b1d290a26
david.beal@akretion.com <david.beal@akretion.com>
2014-09-16 14:51:59 +0200

    [ADD] module price_multicompany

---
+++ price_multicompany/__openerp__.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Author: David BEAL
+#    Copyright 2014 Akretion
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+{
+    'name': 'price_multicompany',
+    'version': '0.1',
+    'category': '',
+    'sequence': 10,
+    'summary': "Changes for multicompany prices",
+    'description': """
+This module should be used if your companies have their currencies non shared.
+Currencies can't be shared because 'res.currency.rate' haven't company_id field
+This also implies that each company must have their own price lists
+    """,
+    'author': 'Akretion',
+    'website': 'http://www.akretion.com',
+    'depends': [
+        'product',
+        ],
+    'data': [
+    ],
+    'demo': [
+    ],
+    'installable': True,
+    'application': False,
+    'images': [
+    ],
+    'css': [
+    ],
+    'js': [
+    ],
+    'qweb': [
+    ],
+}
---
+++ price_multicompany/currency.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#  licence AGPL version 3 or later
+#  see licence in __openerp__.py or http://www.gnu.org/licenses/agpl-3.0.txt
+#  Copyright (C) 2014 Akretion (http://www.akretion.com).
+#  @author David BEAL <david.beal@akretion.com>
+#
+##############################################################################
+
+from openerp.osv import orm, fields
+
+
+class ResCurrency(orm.Model):
+    _inherit = 'res.currency'
+
+    def name_get(self, cr, uid, ids, context=None):
+        """ If currencies are not shared and they have the same name,
+            then display have to distinguish them"""
+        result = []
+        for currency in self.browse(cr, uid, ids, context):
+            cpny = currency.company_id and currency.company_id.name
+            result.append((currency.id, currency.name + ' (' + cpny + ')'))
+        return result
+
+
+class ProductPriceType(orm.Model):
+    _inherit = 'product.price.type'
+
+    _columns = {
+        'currency_id': fields.property(
+            'res.currency',
+            relation='res.currency',
+            type='many2one',
+            string='Currency',
+            view_load=True,
+            required=True,
+            help="The currency the field is expressed in."
+                 "Field redefined in field property "
+                 "for multicompany purpose."),
+    }
