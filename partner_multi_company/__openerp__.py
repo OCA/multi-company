0d56e12872456b5b72852f1da3f319bb1c121a6c
Pedro M. Baeza <pedro.baeza@gmail.com>
2015-12-21 01:01:32 +0100

    partner_multi_company: Tests + README

--- partner_multi_company/__openerp__.py
+++ partner_multi_company/__openerp__.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 # © 2015 Oihane Crucelaegui
+# © 2015 Pedro M. Baeza <pedro.baeza@serviciosbaeza.com>
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
 {
@@ -7,12 +8,16 @@
     "summary": "Select individually the partner visibility on each company",
     "version": "8.0.1.0.0",
     "license": "AGPL-3",
-    "depends": ["base"],
-    "author": "Odoo Community Association (OCA)",
+    "depends": [
+        "base",
+        "base_suspend_security",
+    ],
+    "author": "Serv. Tecnol. Avanzados - Pedro M. Baeza, "
+              "Odoo Community Association (OCA)",
     "contributors": [
         "Oihane Crucelaegui <oihanecruce@avanzosc.es>",
     ],
-    "category": "Custom Module",
+    "category": "Partner Management",
     "data": [
         "views/res_partner_view.xml",
     ],
