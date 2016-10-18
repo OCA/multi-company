dad1d497c231b603bc6fd713099ef6dd6b3999b2
chafique-delli <chafique.delli@akretion.com>
2016-10-18 15:21:07 +0200

    add warehouse_id in the values for create sale order

--- purchase_sale_inter_company/models/res_config.py
+++ purchase_sale_inter_company/models/res_config.py
@@ -13,16 +13,22 @@ class InterCompanyRulesConfig(models.TransientModel):
         string='Sale Orders Auto Validation',
         help='When a Sale Order is created by a multi company rule for '
              'this company, it will automatically validate it.')
+    warehouse_id = fields.Many2one(
+        'stock.warehouse', string='Warehouse For Sale Orders',
+        help='Default value to set on Sale Orders that will be created '
+        'based on Purchase Orders made to this company.')
 
     @api.onchange('company_id')
     def onchange_company_id(self):
         if self.company_id:
             self.sale_auto_validation = self.company_id.sale_auto_validation
+            self.warehouse_id = self.company_id.warehouse_id.id
 
     @api.multi
     def set_inter_company_configuration(self):
         if self.company_id:
             vals = {
-                'sale_auto_validation': self.auto_validation,
+                'sale_auto_validation': self.sale_auto_validation,
+                'warehouse_id': self.warehouse_id.id
             }
             self.company_id.write(vals)
