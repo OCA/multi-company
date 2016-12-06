188bc85ad1ec0f4c25a4ec14b13ec5fe4714868b
Pedro M. Baeza <pedro.baeza@gmail.com>
2016-12-06 17:45:01 +0100

    partner_multi_company: Migration to 9.0

--- partner_multi_company/__openerp__.py
+++ partner_multi_company/__openerp__.py
@@ -1,18 +1,17 @@
 # -*- coding: utf-8 -*-
 # © 2015 Oihane Crucelaegui
-# © 2015 Pedro M. Baeza <pedro.baeza@serviciosbaeza.com>
+# © 2015-2016 Pedro M. Baeza <pedro.baeza@tecnativa.com>
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
 {
     "name": "Partner multi-company",
     "summary": "Select individually the partner visibility on each company",
-    "version": "8.0.1.0.0",
+    "version": "9.0.1.0.0",
     "license": "AGPL-3",
     "depends": [
-        "base",
         "base_suspend_security",
     ],
-    "author": "Serv. Tecnol. Avanzados - Pedro M. Baeza, "
+    "author": "Tecnativa, "
               "Odoo Community Association (OCA)",
     "contributors": [
         "Oihane Crucelaegui <oihanecruce@avanzosc.es>",
--- partner_multi_company/models/res_users.py
+++ partner_multi_company/models/res_users.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# © 2015 Pedro M. Baeza <pedro.baeza@serviciosbaeza.com>
+# © 2015-2016 Pedro M. Baeza <pedro.baeza@tecnativa.com>
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html.html
 
 from openerp import api, models
