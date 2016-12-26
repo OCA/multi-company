6db3490d7a547c03a03f694519e4f5a2a950fc41
Dave Lasley <dave@laslabs.com>
2016-12-26 12:13:33 -0800

    [MIG] product_multi_company: Upgrade to v10 * Rename manifest * Change openerp references to odoo * Bump version * Add pragma no cover * Edit security of product employee to allow writes (in tests) * Fix permissions in tests * Fix domain & add test * Implement base_multi_company on product_multi_company * Add related cols for product variant

--- product_multi_company/tests/test_product_multi_company.py
+++ product_multi_company/tests/test_product_multi_company.py
@@ -2,8 +2,8 @@
 # Copyright 2015-2016 Pedro M. Baeza <pedro.baeza@tecnativa.com>
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
-from openerp.tests import common
-from openerp.exceptions import AccessError
+from odoo.tests import common
+from odoo.exceptions import AccessError
 
 
 @common.at_install(False)
@@ -11,13 +11,29 @@ from openerp.exceptions import AccessError
 class TestProductMultiCompany(common.TransactionCase):
     def setUp(self):
         super(TestProductMultiCompany, self).setUp()
+        self.group_user = self.env.ref('base.group_user')
+        self.env.ref('product.access_product_product_employee').write({
+            'perm_write': True,
+            'perm_read': True,
+            'perm_create': True,
+            'perm_unlink': True,
+        })
+        self.env.ref('product.access_product_template_user').write({
+            'perm_write': True,
+            'perm_read': True,
+            'perm_create': True,
+            'perm_unlink': True,
+        })
         self.company_1 = self.env['res.company'].create(
-            {'name': 'Test company 1'})
+            {'name': 'Test company 1'}
+        )
         self.company_2 = self.env['res.company'].create(
-            {'name': 'Test company 2'})
+            {'name': 'Test company 2'}
+        )
         self.product_company_none = self.env['product.product'].create(
             {'name': 'Product without company',
-             'company_ids': [(6, 0, [])]})
+             'company_ids': [(6, 0, [])],
+             'company_id': False})
         self.product_company_1 = self.env['product.product'].create(
             {'name': 'Product from company 1',
              'company_ids': [(6, 0, self.company_1.ids)]})
@@ -31,14 +47,14 @@ class TestProductMultiCompany(common.TransactionCase):
             {'name': 'User company 1',
              'login': 'user_company_1',
              'groups_id': [
-                 (6, 0, self.env.ref('base.group_sale_manager').ids)],
+                 (6, 0, self.group_user.ids)],
              'company_id': self.company_1.id,
              'company_ids': [(6, 0, self.company_1.ids)]})
         self.user_company_2 = self.env['res.users'].create(
             {'name': 'User company 2',
              'login': 'user_company_2',
              'groups_id': [
-                 (6, 0, self.env.ref('base.group_sale_manager').ids)],
+                 (6, 0, self.group_user.ids)],
              'company_id': self.company_2.id,
              'company_ids': [(6, 0, self.company_2.ids)]})
 
@@ -46,7 +62,7 @@ class TestProductMultiCompany(common.TransactionCase):
         product = self.env['product.product'].create({'name': 'Test'})
         company = self.env['res.company']._company_default_get(
             'product.template')
-        self.assertTrue(company in product.company_ids)
+        self.assertTrue(company.id in product.company_ids.ids)
 
     def test_company_none(self):
         self.assertFalse(self.product_company_none.company_id)
@@ -55,7 +71,10 @@ class TestProductMultiCompany(common.TransactionCase):
         self.product_company_none.sudo(self.user_company_2.id).name = "Test"
 
     def test_company_1(self):
-        self.assertEqual(self.product_company_1.company_id, self.company_1)
+        self.assertEqual(
+            self.product_company_1.sudo(self.user_company_1).company_id,
+            self.company_1,
+        )
         # All of this should be allowed
         self.product_company_1.sudo(self.user_company_1).name = "Test"
         self.product_company_both.sudo(self.user_company_1).name = "Test"
@@ -64,7 +83,10 @@ class TestProductMultiCompany(common.TransactionCase):
             self.product_company_2.sudo(self.user_company_1).name = "Test"
 
     def test_company_2(self):
-        self.assertEqual(self.product_company_2.company_id, self.company_2)
+        self.assertEqual(
+            self.product_company_2.sudo(self.user_company_2).company_id,
+            self.company_2,
+        )
         # All of this should be allowed
         self.product_company_2.sudo(self.user_company_2).name = "Test"
         self.product_company_both.sudo(self.user_company_2).name = "Test"
@@ -76,6 +98,6 @@ class TestProductMultiCompany(common.TransactionCase):
         from ..hooks import uninstall_hook
         uninstall_hook(self.env.cr, None)
         rule = self.env.ref('product.product_comp_rule')
-        domain = (" ['|',('company_id','=',user.company_id.id),"
-                  "('company_id','=',False)]")
+        domain = (" ['|', ('company_id', '=', user.company_id.id), "
+                  "('company_id', '=', False)]")
         self.assertEqual(rule.domain_force, domain)
