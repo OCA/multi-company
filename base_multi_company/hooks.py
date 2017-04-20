6ad39d78f782146b00f0edb57e9f430823197b86
Dave Lasley <dave@laslabs.com>
2017-04-19 19:57:31 -0700

    [ADD] base_multi_company: Create new module * Create new module to provide base multi company logic and mixin * Add deactivation by company mixin * Add company_id/ids handling * Add break after company is found Squashed commits: [854cc36] Increase test coverage [770bd71] Revert hook view create back to model init [40e803e] Fix apples and oranges [7a4dfb4] Use registry correctly [6e9f170] Switch company_id to computed & move company aliased view creation to post init hook [faa4fc9] Remove active functionality [fecfb59] Add explicit tests for active and inactive searches

---
+++ base_multi_company/hooks.py
@@ -0,0 +1,66 @@
+# -*- coding: utf-8 -*-
+# Copyright 2015-2016 Pedro M. Baeza <pedro.baeza@tecnativa.com>
+# Copyright 2017 LasLabs Inc.
+# License LGPL-3 - See http://www.gnu.org/licenses/lgpl-3.0.html
+
+from odoo import api, SUPERUSER_ID
+
+
+__all__ = [
+    'post_init_hook',
+    'uninstall_hook',
+]
+
+
+def post_init_hook(cr, rule_ref, model_name):
+    """ Set the `domain_force` and default `company_ids` to `company_id`.
+
+    Args:
+        cr (Cursor): Database cursor to use for operation.
+        rule_ref (string): XML ID of security rule to write the
+            `domain_force` from.
+        model_name (string): Name of Odoo model object to search for
+            existing records.
+    """
+    with api.Environment.manage():
+        env = api.Environment(cr, SUPERUSER_ID, {})
+        # Change access rule
+        rule = env.ref(rule_ref)
+        rule.write({
+            'active': True,
+            'domain_force': (
+                "['|', ('company_ids', 'in', user.company_id.ids),"
+                " ('company_id', '=', False)]"
+            ),
+        })
+        # Copy company values
+        model = env[model_name]
+        groups = model.read_group([], ['company_id'], ['company_id'])
+        for group in groups:
+            if not group['company_id']:
+                continue
+            records = model.search(group['__domain'])
+            records.write({
+                'company_ids': [(6, 0, [group['company_id'][0]])],
+            })
+
+
+def uninstall_hook(cr, rule_ref):
+    """ Restore product rule to base value.
+
+    Args:
+        cr (Cursor): Database cursor to use for operation.
+        rule_ref (string): XML ID of security rule to remove the
+            `domain_force` from.
+    """
+    with api.Environment.manage():
+        env = api.Environment(cr, SUPERUSER_ID, {})
+        # Change access rule
+        rule = env.ref(rule_ref)
+        rule.write({
+            'active': False,
+            'domain_force': (
+                " ['|', ('company_id', '=', user.company_id.id),"
+                " ('company_id', '=', False)]"
+            ),
+        })
