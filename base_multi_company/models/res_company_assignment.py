fd8558d329871d5257424958631030d248dab7b8
Dave Lasley <dave@laslabs.com>
2017-05-05 07:35:19 -0700

    PR fixes (+1 squashed commit) Squashed commits: [4c17d04] auto_join company_ids

--- base_multi_company/models/res_company_assignment.py
+++ base_multi_company/models/res_company_assignment.py
@@ -6,6 +6,14 @@ from odoo import fields, models
 
 
 class ResCompanyAssignment(models.Model):
+    """ This model circumvents company domains to allow assignments.
+
+    Normally when using a multi company setup, you are restricted to seeing
+    only records owned by the company your user is operating under
+    (`user.company_id`). This creates a catch 22 with multi-company records,
+    because in order to assign to another company you have to be able to view
+    that company.
+    """
 
     _name = 'res.company.assignment'
     _auto = False
