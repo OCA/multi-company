178ee0a9e92ed1eac0f11e45dfe30881c954452a
Laurent Mignon (ACSONE) <laurent.mignon@acsone.eu>
2017-05-05 16:33:41 +0200

    10.0 base multi company lmi (#1)
    * Revert "Revert hook view create back to model init"
    
    This reverts commit 770bd71a919e5801702f0fd9d398a37c08472a70.
    
    * [FIX] base_multi_company: Always create the view into a pre_init_hook to avoid error in log

--- base_multi_company/models/res_company_assignment.py
+++ base_multi_company/models/res_company_assignment.py
@@ -2,7 +2,7 @@
 # Copyright 2017 LasLabs Inc.
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
 
-from odoo import api, fields, models, tools
+from odoo import fields, models
 
 
 class ResCompanyAssignment(models.Model):
@@ -11,12 +11,3 @@ class ResCompanyAssignment(models.Model):
     _auto = False
 
     name = fields.Char()
-
-    @api.model_cr
-    def init(self):
-        tools.drop_view_if_exists(self.env.cr, self._table)
-        self.env.cr.execute("""
-            CREATE OR REPLACE VIEW %s
-                AS SELECT id, name
-                FROM res_company;
-        """ % (self._table))
