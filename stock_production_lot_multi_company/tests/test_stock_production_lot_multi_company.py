07053f6b40307b95ef6ae616e598a7da4379bb00
Roberto Onnis <roberto.onnis@innoviu.com>
2017-06-01 10:18:34 +0200

    Mig stock_production_lot [10.0] WIP

--- stock_production_lot_multi_company/tests/test_stock_production_lot_multi_company.py
+++ stock_production_lot_multi_company/tests/test_stock_production_lot_multi_company.py
@@ -2,8 +2,8 @@
 # (c) 2015 Ainara Galdona - AvanzOSC
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
-from openerp.osv.orm import except_orm
-from openerp.tests.common import TransactionCase
+from odoo.tests.common import TransactionCase
+from odoo.exceptions import AccessError
 
 
 class TestStockProductionLotMultiCompany(TransactionCase):
@@ -21,8 +21,11 @@ class TestStockProductionLotMultiCompany(TransactionCase):
 
         # companies
         self.main_comp = self.env.ref('base.main_company')
-        self.secondary_company = self.company_model.create({'name':
-                                                            'SecondComp'})
+        self.secondary_company = self.company_model.create(
+            {
+                'name':'SecondComp',
+                'account_no': '123456'
+            })
 
         # user groups
         self.lot_group = self.env.ref('stock.group_production_lot')
@@ -35,18 +38,17 @@ class TestStockProductionLotMultiCompany(TransactionCase):
              'login': 'secuser',
              'email': 'secuser@youcompany.com',
              'company_id': self.secondary_company.id,
-             'company_ids': [(6, 0, [self.secondary_company.id])],
+             'company_ids': [(6, 0, self.secondary_company.ids)],
              'groups_id': [(6, 0, [self.stock_manager_group.id,
-                                   self.lot_group.id,
-                                   self.group_multi_company.id])],
+                                   self.lot_group.id])],
              })
         self.main_user = self.user_model.create(
             {'name': 'Main company user',
              'login': 'mainuser',
              'email': 'mainuser@youcompany.com',
              'company_id': self.main_comp.id,
-             'company_ids': [(6, 0, [self.main_comp.id,
-                                     self.secondary_company.id])],
+             'company_ids': [(6, 0, (self.main_comp +
+                                     self.secondary_company).ids)],
              'groups_id': [(6, 0, [self.stock_manager_group.id,
                                    self.lot_group.id,
                                    self.group_multi_company.id])],
@@ -56,8 +58,8 @@ class TestStockProductionLotMultiCompany(TransactionCase):
              'login': 'multicompuser',
              'email': 'multicompuser@youcompany.com',
              'company_id': self.main_comp.id,
-             'company_ids': [(6, 0, [self.main_comp.id,
-                                     self.secondary_company.id])],
+             'company_ids': [(6, 0, (self.main_comp +
+                                     self.secondary_company).ids)],
              'groups_id': [(6, 0, [self.stock_manager_group.id,
                                    self.lot_group.id,
                                    self.group_multi_company.id])],
@@ -68,24 +70,22 @@ class TestStockProductionLotMultiCompany(TransactionCase):
         self.main_comp_product = self.product_model.create(
             {'name': 'Main company product',
              'default_code': '[MCP]',
-             'company_id': self.main_comp.id,
-             'track_all': True,
+             'company_ids': [(6, 0, self.main_comp.ids)],
              'uom_id': self.unit_uom.id})
         self.second_comp_product = self.product_model.create(
             {'name': 'Secondary company product',
              'default_code': '[SCP]',
-             'company_id': self.secondary_company.id,
-             'track_all': True,
+             'company_ids': [(6, 0, self.secondary_company.ids)],
              'uom_id': self.unit_uom.id})
 
-    def test_lot_main_creation(self):
+    def test_00_lot_main_creation(self):
         main_lot = self.lot_model.sudo(self.main_user.id).create(
             {'name': 'MAINLOT', 'product_id': self.main_comp_product.id})
         self.assertEqual(main_lot.company_id, self.main_comp,
                          'Incorrect company for the created lot in main '
                          'company user.')
 
-    def test_lot_secondary_creation(self):
+    def test_01_lot_secondary_creation(self):
         secondary_lot = self.lot_model.sudo(self.second_user.id).create(
             {'name': 'SECONDARYLOT',
              'product_id': self.second_comp_product.id})
@@ -93,7 +93,7 @@ class TestStockProductionLotMultiCompany(TransactionCase):
                          'Incorrect company for the created lot in secondary '
                          'company user.')
 
-    def test_lot_multicomp_creation(self):
+    def test_02_lot_multicomp_creation(self):
         multicompuser_lot = self.lot_model.sudo(self.multicomp_user.id).create(
             {'name': 'MULTICOMPUSERLOT',
              'product_id': self.second_comp_product.id})
@@ -101,9 +101,9 @@ class TestStockProductionLotMultiCompany(TransactionCase):
                          'Incorrect company for the created lot in '
                          'multicompany company user.')
 
-    def test_error_lot_creation(self):
-        with self.assertRaises(except_orm):
+    def test_03_error_lot_creation(self):
+        with self.assertRaises(AccessError):
             self.lot_model.sudo(self.second_user.id).create(
                 {'name': 'ERRORLOT',
                  'product_id': self.second_comp_product.id,
-                 'company_id': self.main_comp.id})
+                 'company_ids': [(6, 0, self.main_comp.ids)]})
