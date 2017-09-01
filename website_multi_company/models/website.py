726da921c91990864e6f31c352623b774cfa92cc
Dave Lasley <dave@laslabs.com>
2017-09-01 10:33:57 -0700

    Better logic - the last stuff sucked functionally

--- website_multi_company/models/website.py
+++ website_multi_company/models/website.py
@@ -3,30 +3,50 @@
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
 
 from odoo import api, fields, models
+from odoo.exceptions import ValidationError
 
 
 class Website(models.Model):
     _inherit = 'website'
 
+    @api.multi
+    @api.constrains('user_id')
+    def _check_user_id(self):
+        for record in self:
+            existing = self.search([('user_id', '=', record.user_id.id)])
+            if len(existing) > 1:
+                raise ValidationError(_(
+                    'You cannot assign the same public user to two websites.',
+                ))
+
     @api.model
     def create(self, vals):
-        record = super(Website, self).create(vals)
-        record._sync_public_user()
-        return record
+        if not vals.get('user_id'):
+            user = self._get_public_user()
+            company_id = vals.get('company_id') or self.env.user.company_id.id
+            if user.company_id.id != company_id:
+                user.company_id = company_id
+            vals['user_id'] = user.id
+        return super(Website, self).create(vals)
 
     @api.multi
     def write(self, vals):
+        if vals.get('user_id'):
+            raise ValidationError(_(
+                'The website public user is maintained automatically and '
+                'cannot be set.',
+            ))
+        if vals.get('company_id'):
+            for record in self:
+                if record.user_id.company_id.id != vals['company_id']:
+                    record.user_id.company_id = vals['company_id']
         res = super(Website, self).write(vals)
-        self._sync_public_user()
         return res
 
-    @api.multi
-    def _sync_public_user(self):
-        for record in self:
-            if record.company_id == record.user_id.company_id:
-                continue
-            public_user = self.env.ref('base.public_user')
-            if public_user.company_id != record.company_id:
-                public_user = public_user.copy()
-                public_user.company_id = record.company_id.id
-            record.user_id = public_user.id
+    @api.model
+    def _get_public_user(self):
+        public_user = self.env.ref('base.public_user')
+        existing = self.search([('user_id', '=', public_user.id)])
+        if not existing:
+            return public_user
+        return public_user.copy()
--- website_multi_company/tests/test_website.py
+++ website_multi_company/tests/test_website.py
@@ -2,18 +2,9 @@
 # Copyright 2017 LasLabs Inc.
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
 
-from odoo.tests import HttpCase
+from odoo.tests import TransactionCase
 
-
-class TestUi(HttpCase):
+class TestWebsite(TransactionCase):
 
     def setUp(self):
-        super(TestUi, self).setUp()
-
-    def test_ui_web(self):
-        """Test backend tests."""
-        self.phantom_js(
-            "/web/tests?module=web_responsive",
-            "",
-            login="admin",
-        )
+        super(TestWebsite, self).setUp()
