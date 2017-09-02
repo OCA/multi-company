139040283346d7bc277672a302359fb1a33e1967
Dave Lasley <dave@laslabs.com>
2017-09-01 17:22:53 -0700

    [ADD] website_product_multi_company: Glue module

---
+++ website_product_multi_company/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 LasLabs Inc.
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
+
+{
+    "name": "Website Product - Multi-Company",
+    "summary": "Install this to isolate your website products by company.",
+    "version": "10.0.1.0.0",
+    "category": "Website",
+    "website": "https://laslabs.com/",
+    "author": "LasLabs, Odoo Community Association (OCA)",
+    "license": "LGPL-3",
+    "installable": True,
+    "depends": [
+        "product_multi_company",
+        "website_multi_company",
+    ],
+}
