7b5169377c42d21bad9986bc33821d9d1a8fd573
chafique delli <chafique.delli@akretion.com>
2018-01-12 18:01:48 +0100

    [10.0] [MIG] account_invoice_inter_company (#71)
    * [10.0]Migrate account_invoice_inter_company module to v10
    
    * refactor code for play onchange method
    
    * active demo datas and fix account_invoice.py, res_config.py, README.rst file
    
    * extract the search domain of users from the _check_intercompany_product method
    
    * fix test
    
    * fix test and update french translation
    
    * rename openerp to odoo
    
    * fix test_flake8 and fix views after David's comment
    
    * add oca_dependencies.txt
    
    * fix bare except
    
    * improve action_cancel method
    
    * fix chart_template_id in tests
    
    * fix transfer_account_id in tests
    
    * fix chart_template_id for company B in tests
    
    * improve tests
    
    * fix tests taking into account the base_multi_company module
    
    * fix tests and contributors after lasley's comment
    
    * improve tests
    
    * fix account_invoice.py after mourad's comment
    
    * fix tests after lasley's comment
    
    * fix transfer account template in demo data
    
    * increases test coverage
    
    * improve action_invoice_open method after astirpe's comment
    
    * fix value for chart_template_id in tests

---
+++ account_invoice_inter_company/__manifest__.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+# © 2013-2014 Odoo SA
+# © 2015-2017 Chafique Delli <chafique.delli@akretion.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    'name': 'Inter Company Module for Invoices',
+    'summary': 'Intercompany invoice rules',
+    'version': '10.0.1.0.0',
+    'category': 'Accounting & Finance',
+    'website': 'http://www.odoo.com',
+    'author': 'Odoo SA, Akretion, Odoo Community Association (OCA)',
+    'license': 'AGPL-3',
+    'installable': True,
+    'depends': [
+        'account_accountant',
+        'onchange_helper',
+    ],
+    'data': [
+        'views/res_company_view.xml',
+        'views/res_config_view.xml',
+    ],
+    'demo': [
+        'demo/inter_company_invoice.xml',
+    ],
+}
