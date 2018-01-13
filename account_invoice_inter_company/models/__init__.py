b34e0fa832c49b5e4d8fae723f1c10f456736e2a
Andrea <a.stirpe@onestein.nl>
2018-01-13 12:01:39 +0100

    [11.0][MIG] Port account_invoice_inter_company + refactoring

--- account_invoice_inter_company/models/__init__.py
+++ account_invoice_inter_company/models/__init__.py
@@ -1,8 +1,7 @@
-# -*- coding: utf-8 -*-
-# © 2013-2014 Odoo SA
-# © 2015-2017 Chafique Delli <chafique.delli@akretion.com>
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+# Copyright 2013-2014 Odoo SA
+# Copyright 2015-2017 Chafique Delli <chafique.delli@akretion.com>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
-from . import res_config
+from . import res_config_settings
 from . import res_company
 from . import account_invoice
