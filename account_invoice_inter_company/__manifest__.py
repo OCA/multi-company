bdbef7c7d3872f3acf06a8e1e55206f5fb0e7eb8
Andrea <a.stirpe@onestein.nl>
2018-01-13 12:01:39 +0100

    [11.0][MIG] Port account_invoice_inter_company + refactoring

--- account_invoice_inter_company/__manifest__.py
+++ account_invoice_inter_company/__manifest__.py
@@ -1,26 +1,24 @@
-# -*- coding: utf-8 -*-
-# © 2013-2014 Odoo SA
-# © 2015-2017 Chafique Delli <chafique.delli@akretion.com>
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+# Copyright 2013-2014 Odoo SA
+# Copyright 2015-2017 Chafique Delli <chafique.delli@akretion.com>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 {
     'name': 'Inter Company Module for Invoices',
     'summary': 'Intercompany invoice rules',
-    'version': '10.0.1.0.0',
+    'version': '11.0.1.0.0',
     'category': 'Accounting & Finance',
-    'website': 'http://www.odoo.com',
+    'website': 'https://github.com/OCA/multi-company',
     'author': 'Odoo SA, Akretion, Odoo Community Association (OCA)',
     'license': 'AGPL-3',
-    'installable': True,
     'depends': [
-        'account_accountant',
+        'account',
         'onchange_helper',
     ],
     'data': [
-        'views/res_company_view.xml',
-        'views/res_config_view.xml',
+        'views/res_config_settings_view.xml',
     ],
     'demo': [
         'demo/inter_company_invoice.xml',
     ],
+    'installable': True,
 }
