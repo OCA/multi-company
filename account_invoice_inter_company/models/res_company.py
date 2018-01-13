44010097feb074e91188677a5d1ac0d775fdc583
Andrea <a.stirpe@onestein.nl>
2018-01-13 12:01:39 +0100

    [11.0][MIG] Port account_invoice_inter_company + refactoring

--- account_invoice_inter_company/models/res_company.py
+++ account_invoice_inter_company/models/res_company.py
@@ -1,4 +1,5 @@
-# -*- coding: utf-8 -*-
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
 from odoo import api, fields, models
 
 
@@ -7,13 +8,16 @@ class ResCompany(models.Model):
     _inherit = 'res.company'
 
     invoice_auto_validation = fields.Boolean(
-        string='Invoice Auto Validation',
         help="When an invoice is created by a multi company rule "
              "for this company, it will automatically validate it",
         default=True)
 
-    @api.model
-    def _find_company_from_partner(self, partner_id):
-        company = self.sudo().search([('partner_id', '=', partner_id)],
-                                     limit=1)
-        return company or False
+    @api.multi
+    def _get_user_domain(self):
+        self.ensure_one()
+        group_account_invoice = self.env.ref('account.group_account_invoice')
+        return [
+            ('id', '!=', self.env.ref('base.user_root').id),
+            ('company_id', '=', self.id),
+            ('id', 'in', group_account_invoice.users.ids),
+        ]
