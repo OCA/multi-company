017dc5dd78af47e6844d12a7c28608b196bbac2a
chafique-delli <chafique.delli@akretion.com>
2018-04-10 14:51:16 +0200

    fix _link_invoice_purchase method and purchase_order_form view

--- purchase_sale_inter_company/models/account_invoice.py
+++ purchase_sale_inter_company/models/account_invoice.py
@@ -8,31 +8,22 @@ class AccountInvoice(models.Model):
     _inherit = 'account.invoice'
 
     @api.multi
-    def inter_company_create_invoice(
+    def _inter_company_create_invoice(
             self, dest_company, dest_inv_type, dest_journal_type):
-        res = super(AccountInvoice, self).inter_company_create_invoice(
+        res = super(AccountInvoice, self)._inter_company_create_invoice(
             dest_company, dest_inv_type, dest_journal_type)
-        if dest_inv_type == 'in_invoice':
-            # Link intercompany purchase order with intercompany invoice
+        if dest_inv_type in ('in_invoice', 'out_refund'):
+            # Link intercompany purchase order with purchase invoice created
             self._link_invoice_purchase(res['dest_invoice'])
         return res
 
     @api.multi
     def _link_invoice_purchase(self, dest_invoice):
         self.ensure_one()
-        sales = self.env['sale.order'].search([
-            ('invoice_ids', '=', self.id),
-            ('auto_purchase_order_id', '!=', False)])
-        for sale in sales:
-            purchase = sale.auto_purchase_order_id.sudo()
-            purchase.invoice_ids = [(4, dest_invoice.id)]
-            if dest_invoice.state not in ['draft', 'cancel']:
-                        purchase.order_line.write({'invoiced': True})
-            for sale_line in sale.order_line:
-                purchase_line = (sale_line.auto_purchase_line_id.
-                                 sudo())
-                for invoice_line in dest_invoice.invoice_line:
-                    if (sale_line.invoice_lines == invoice_line.
-                            auto_invoice_line_id):
-                        purchase_line.invoice_lines = [
-                            (4, invoice_line.id)]
+        for dest_invoice_line in dest_invoice.invoice_line_ids:
+            purchase_order_line_ids = dest_invoice_line.\
+                auto_invoice_line_id.sale_line_ids.\
+                mapped('auto_purchase_line_id')
+            if purchase_order_line_ids:
+                purchase_order_line_ids.invoice_lines = [
+                    (6, 0, [dest_invoice_line.id])]
