0fd411d84cd988fefbcf5d2cbae16bbce06bfeeb
Carlos Dauden <carlos.dauden@tecnativa.com>
2018-04-24 19:11:43 +0200

    [MIG] purchase_sale_inter_company: Migration to 11.0 (from 8.0)

--- purchase_sale_inter_company/models/res_company.py
+++ purchase_sale_inter_company/models/res_company.py
@@ -1,21 +1,27 @@
-# -*- coding: utf-8 -*-
-# © 2013-Today Odoo SA
-# © 2016 Chafique DELLI @ Akretion
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+# Copyright 2013-Today Odoo SA
+# Copyright 2016 Chafique DELLI @ Akretion
+# Copyright 2018 Tecnativa - Carlos Dauden
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
-from openerp import fields, models
+from odoo import fields, models
 
 
 class ResCompany(models.Model):
 
     _inherit = 'res.company'
 
+    so_from_po = fields.Boolean(
+        string="Create Sale Orders when buying to this company",
+        help='Generate a Sale Order when a Purchase Order with this company '
+        'as supplier is created.\n The intercompany user must at least be '
+        'Sale User.',
+    )
     sale_auto_validation = fields.Boolean(
         string='Sale Auto Validation',
-        help="When a Sale Order is created by a multi company rule "
-             "for this company, it will automatically validate it",
+        help='When a Sale Order is created by a multi company rule '
+             'for this company, it will automatically validate it',
         default=True)
     warehouse_id = fields.Many2one(
         'stock.warehouse', string='Warehouse For Sale Orders',
-        help="Default value to set on Sale Orders that "
-        "will be created based on Purchase Orders made to this company")
+        help='Default value to set on Sale Orders that '
+        'will be created based on Purchase Orders made to this company')
