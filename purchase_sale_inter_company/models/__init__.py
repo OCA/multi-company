da4dcb57a792a6bb3b61ce2b8bddb23b7e79c859
Carlos Dauden <carlos.dauden@tecnativa.com>
2018-04-24 19:11:43 +0200

    [MIG] purchase_sale_inter_company: Migration to 11.0 (from 8.0)

--- purchase_sale_inter_company/models/__init__.py
+++ purchase_sale_inter_company/models/__init__.py
@@ -1,6 +1,6 @@
-# -*- coding: utf-8 -*-
-from . import res_config
-from . import res_company
-from . import purchase_order
-from . import sale_order
 from . import account_invoice
+from . import purchase_order
+from . import res_company
+from . import res_config
+from . import sale_order
+from . import stock_picking
