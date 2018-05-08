d8483fee38d56eb615e8b7fddfebca6fcaf0050b
cubells <vicent@vcubells.net>
2018-05-08 18:04:30 +0200

    [MIG] product_tax_multicompany_default: Migration to 11.0
    [UPD] Update product_tax_multicompany_default.pot
    
    [UPD] Update product_tax_multicompany_default.pot
    
    Update translation files
    
    Updated by Update PO files to match POT (msgmerge) hook in Weblate.
    
    [ADD] icon.png

--- product_tax_multicompany_default/tests/test_product_tax_multicompany.py
+++ product_tax_multicompany_default/tests/test_product_tax_multicompany.py
@@ -1,8 +1,8 @@
-# -*- coding: utf-8 -*-
 # Copyright 2017 Carlos Dauden - Tecnativa <carlos.dauden@tecnativa.com>
+# Copyright 2018 Vicent Cubells - Tecnativa <vicent.cubells@tecnativa.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from openerp.tests.common import SavepointCase
+from odoo.tests.common import SavepointCase
 
 
 class TestsProductTaxMulticompany(SavepointCase):
@@ -13,23 +13,20 @@ class TestsProductTaxMulticompany(SavepointCase):
             {'name': 'Test company 1'})
         cls.company_2 = cls.env['res.company'].create(
             {'name': 'Test company 2'})
-        group_sale_manager = cls.env.ref('base.group_sale_manager')
         group_account_manager = cls.env.ref('account.group_account_manager')
         ResUsers = cls.env['res.users']
         cls.user_1 = ResUsers.create(
             {'name': 'User not admin',
              'login': 'user_1',
              'email': 'test@test.com',
-             'groups_id': [(6, 0, (
-                 group_sale_manager | group_account_manager).ids)],
+             'groups_id': [(6, 0, group_account_manager.ids)],
              'company_id': cls.company_1.id,
              'company_ids': [(6, 0, cls.company_1.ids)]})
         cls.user_2 = ResUsers.create(
             {'name': 'User not admin',
              'login': 'user_2',
              'email': 'test@test.com',
-             'groups_id': [(6, 0, (
-                 group_sale_manager | group_account_manager).ids)],
+             'groups_id': [(6, 0, group_account_manager.ids)],
              'company_id': cls.company_2.id,
              'company_ids': [(6, 0, cls.company_2.ids)]})
         AccountTax = cls.env['account.tax']
@@ -60,19 +57,31 @@ class TestsProductTaxMulticompany(SavepointCase):
         })
         cls.tax_20_sc1 = AccountTax.sudo(cls.user_1.id).create(tax_vals)
         cls.tax_20_sc2 = AccountTax.sudo(cls.user_2.id).create(tax_vals)
-        IrValues = cls.env['ir.values']
-        IrValues.set_default(
-            'product.template', "taxes_id", [cls.tax_10_cc1.id],
-            for_all_users=True, company_id=cls.company_1.id)
-        IrValues.set_default(
-            'product.template', "supplier_taxes_id", [cls.tax_10_sc1.id],
-            for_all_users=True, company_id=cls.company_1.id)
-        IrValues.set_default(
-            'product.template', "taxes_id", [cls.tax_20_cc2.id],
-            for_all_users=True, company_id=cls.company_2.id)
-        IrValues.set_default(
-            'product.template', "supplier_taxes_id", [cls.tax_20_sc2.id],
-            for_all_users=True, company_id=cls.company_2.id)
+        IrDefault = cls.env['ir.default']
+        IrDefault.set(
+            model_name='product.template',
+            field_name="taxes_id",
+            value=[cls.tax_10_cc1.id],
+            company_id=cls.company_1.id,
+        )
+        IrDefault.set(
+            model_name='product.template',
+            field_name="supplier_taxes_id",
+            value=[cls.tax_10_sc1.id],
+            company_id=cls.company_1.id,
+        )
+        IrDefault.set(
+            model_name='product.template',
+            field_name="taxes_id",
+            value=[cls.tax_20_cc2.id],
+            company_id=cls.company_2.id,
+        )
+        IrDefault.set(
+            model_name='product.template',
+            field_name="supplier_taxes_id",
+            value=[cls.tax_20_sc2.id],
+            company_id=cls.company_2.id,
+        )
 
     def test_multicompany_default_tax(self):
         product = self.env['product.product'].sudo(self.user_1.id).create({
@@ -108,14 +117,18 @@ class TestsProductTaxMulticompany(SavepointCase):
         self.assertIn(self.tax_10_sc1, product.supplier_taxes_id)
 
     def test_tax_not_default_set_match(self):
-        IrValues = self.env['ir.values']
-        IrValues.set_default(
-            'product.template', "taxes_id", [self.tax_20_cc1.id],
-            for_all_users=True, company_id=self.company_1.id)
-        IrValues.set_default(
-            'product.template', "supplier_taxes_id", [self.tax_20_sc1.id],
-            for_all_users=True, company_id=self.company_1.id)
-
+        IrDefault = self.env['ir.default']
+        IrDefault.set(
+            model_name='product.template',
+            field_name="taxes_id",
+            value=[self.tax_20_cc1.id],
+            company_id=self.company_1.id)
+        IrDefault.set(
+            model_name='product.template',
+            field_name="supplier_taxes_id",
+            value=[self.tax_20_sc1.id],
+            company_id=self.company_1.id,
+        )
         product = self.env['product.product'].sudo(self.user_1.id).create({
             'name': 'Test Product',
             'taxes_id': [(6, 0, self.tax_10_cc1.ids)],
