086aa1a0816994f21333756b7f656ac0ca798efe
cubells <vicent@vcubells.net>
2018-05-08 18:04:30 +0200

    [MIG] product_tax_multicompany_default: Migration to 11.0
    [UPD] Update product_tax_multicompany_default.pot
    
    [UPD] Update product_tax_multicompany_default.pot
    
    Update translation files
    
    Updated by Update PO files to match POT (msgmerge) hook in Weblate.
    
    [ADD] icon.png

--- product_tax_multicompany_default/__manifest__.py
+++ product_tax_multicompany_default/__manifest__.py
@@ -1,10 +1,10 @@
-# -*- coding: utf-8 -*-
 # Copyright 2017 Carlos Dauden - Tecnativa <carlos.dauden@tecnativa.com>
+# Copyright 2018 Vicent Cubells - Tecnativa <vicent.cubells@tecnativa.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     'name': 'Product Tax Multi Company Default',
-    'version': '9.0.1.0.0',
+    'version': '11.0.1.0.0',
     'category': 'Account',
     'website': 'https://www.tecnativa.com',
     'author': 'Tecnativa, '
