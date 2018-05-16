6ae5f438a715e88691d7cf0f83c6204003a8d354
Marçal Isern <marsal.isern@qubiq.es>
2018-05-16 19:40:23 +0200

    [MIG] stock_production_log_multi_company: Migration to 11.0

--- stock_production_lot_multi_company/__manifest__.py
+++ stock_production_lot_multi_company/__manifest__.py
@@ -1,18 +1,18 @@
-# -*- coding: utf-8 -*-
 # Copyright 2015 Ainara Galdona - AvanzOSC
 # Copyright 2017 Lorenzo Battistini - Agile Business Group
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-
 {
     "name": "Stock Production Lot Multi Company",
     "summary": "Make serial numbers multi-company aware",
-    "version": "10.0.1.0.0",
+    "version": "11.0.1.0.0",
     "category": "Stock",
+    "website": 'https://github.com/OCA/multi-company',
     "license": "AGPL-3",
     "author": "OdooMRP team, "
               "AvanzOSC, "
               "Serv. Tecnol. Avanzados - Pedro M. Baeza, "
               "Odoo Community Association (OCA)",
+    "installable": True,
     "depends": [
         "stock",
     ],
@@ -20,5 +20,4 @@
         "security/stock_production_lot_security.xml",
         "views/stock_production_lot_view.xml",
     ],
-    'installable': True,
 }
