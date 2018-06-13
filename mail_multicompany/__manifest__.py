e05f3adf5e6eb0b6c5293adf384569ed857bcc74
jesusVMayor <jesus@comunitea.com>
2018-06-13 10:53:02 +0200

    [ADD]mail_multicompany: Added module.

---
+++ mail_multicompany/__manifest__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    'name': 'Email Gateway Multi company',
+    'version': '10.0.0.1.0',
+    'author': 'Comunitea, Odoo Community Association (OCA)',
+    'license': 'AGPL-3',
+    'depends': ['mail'],
+    'category': 'Extra Tools',
+    'website': 'https://github.com/OCA/server-tools',
+    'data': ['security/mail_security.xml',
+             'views/ir_mail_server_view.xml'],
+    'installable': True,
+}
