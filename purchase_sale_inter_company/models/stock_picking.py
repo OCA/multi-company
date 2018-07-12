45e516853e86ef1a1526aa1f3219269e8a83ca3d
Pedro M. Baeza <pedro.baeza@gmail.com>
2018-07-12 09:49:34 +0200

    [FIX] purchase_sale_inter_company: Transfer dropship pickings correctly

--- purchase_sale_inter_company/models/stock_picking.py
+++ purchase_sale_inter_company/models/stock_picking.py
@@ -1,4 +1,5 @@
 # Copyright 2018 Tecnativa - Carlos Dauden
+# Copyright 2018 Tecnativa - Pedro M. Baeza
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 from odoo import _, api, fields, models
@@ -19,12 +20,9 @@ class StockPicking(models.Model):
             purchase = pick.sale_id.auto_purchase_order_id
             if not purchase:
                 continue
+            purchase.picking_ids.write({'intercompany_picking_id': pick.id})
             for move_line in pick.move_line_ids:
                 qty_done = move_line.qty_done
-                # po_move_lines = purchase.sudo().picking_ids.mapped(
-                #     'move_line_ids').filtered(
-                #         lambda x: x.product_id == move_line.product_id and
-                #         not x.processed_boolean)
                 po_move_lines = move_line.move_id.sale_line_id.\
                     auto_purchase_line_id.move_ids.mapped('move_line_ids')
                 for po_move_line in po_move_lines:
@@ -38,17 +36,13 @@ class StockPicking(models.Model):
                 if qty_done and po_move_lines:
                     po_move_lines[-1:].qty_done += qty_done
                 elif not po_move_lines:
-                    raise UserError(
-                        _('Any picking to assign units from %s (%s)') %
-                        (pick.name, purchase.name))
-                    # TODO: Create new DropShip Picking
-            pick.intercompany_picking_id = po_move_line.picking_id
-            po_move_line.picking_id.intercompany_picking_id = pick
-        # Done dropship pickings
-        if po_picks:
-            po_self = self.sudo().with_context(
-                force_company=po_picks[0].company_id.id)
-            for po_pick in po_picks:
-                po_self.env['stock.backorder.confirmation'].create(
-                    {'pick_id': po_pick.id}).process()
+                    raise UserError(_(
+                        "There's no corresponding line in PO %s for assigning "
+                        "qty from %s for product %s"
+                    ) % (purchase.name, pick.name, move_line.product_id.name))
+        # Transfer dropship pickings
+        for po_pick in po_picks.sudo():
+            po_pick.with_context(
+                force_company=po_pick.company_id.id,
+            ).action_done()
         return super(StockPicking, self).do_transfer()
