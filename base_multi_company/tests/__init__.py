4b60e7b0ea5fa52e4c6771b2c82014f653e04883
Rodrigo Ferreira <rodrigosferreira91@gmail.com>
2018-07-31 10:03:22 -0300

    [MIG] base_multi_company: Migration to 11.0

--- base_multi_company/tests/__init__.py
+++ base_multi_company/tests/__init__.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # License LGPL-3 - See http://www.gnu.org/licenses/lgpl-3.0.html
 
 from . import test_multi_company_abstract
--- base_multi_company/tests/test_res_company_assignment.py
+++ base_multi_company/tests/test_res_company_assignment.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Copyright 2017 LasLabs Inc.
 # License LGPL-3 - See http://www.gnu.org/licenses/lgpl-3.0.html
 
