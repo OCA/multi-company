f13b267942faec0755ce8ae30fb4c3bdb7534d6a
Cédric Pigeon <cedric.pigeon@acsone.eu>
2018-09-18 16:52:20 +0200

    [ADD][FIX] fix some stuffs and add unit tests

--- mail_multicompany/models/mail_message.py
+++ mail_multicompany/models/mail_message.py
@@ -11,15 +11,20 @@ class MailMessage(models.Model):
 
     @api.model
     def create(self, vals):
-        if not vals.get('company_id') and vals['author_id']:
-            author_user = self.env['res.users'].search(
-                [('partner_id', '=', vals['author_id'])])
-            vals['company_id'] = author_user.company_id.id
+        if not vals.get('company_id'):
+            if vals.get('author_id', False):
+                author_user = self.env['res.users'].search(
+                    [('partner_id', '=', vals['author_id'])])
+                company_id = author_user.company_id.id
+            else:
+                company_id = self.env.user.company_id.id
+            vals['company_id'] = company_id
         if vals.get('model') and vals.get('res_id'):
-            object = self.env[vals['model']].browse(vals['res_id'])
-            if 'company_id' in object._fields:
-                vals['company_id'] = object.company_id.id
+            current_object = self.env[vals['model']].browse(vals['res_id'])
+            if hasattr(current_object, 'company_id'):
+                vals['company_id'] = current_object.company_id.id
         if not vals.get('mail_server_id'):
             vals['mail_server_id'] = self.sudo().env['ir.mail_server'].search(
-                [('company_id', '=', vals.get('company_id', False))]).id
+                [('company_id', '=', vals.get('company_id', False))],
+                order='sequence', limit=1).id
         return super(MailMessage, self).create(vals)
