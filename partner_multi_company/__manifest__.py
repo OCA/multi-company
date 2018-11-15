5035d63effb49943469ae7902ed975cf72cfbf22
cubells <vicent@vcubells.net>
2018-11-15 10:55:57 +0100

    [MIG] partner_multi_company: Migration to 11.0

--- partner_multi_company/__manifest__.py
+++ partner_multi_company/__manifest__.py
@@ -1,12 +1,11 @@
-# -*- coding: utf-8 -*-
-# © 2015 Oihane Crucelaegui
-# © 2015-2016 Pedro M. Baeza <pedro.baeza@tecnativa.com>
+# Copyright 2015 Oihane Crucelaegui
+# Copyright 2015-2016 Pedro M. Baeza <pedro.baeza@tecnativa.com>
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
 {
     "name": "Partner multi-company",
     "summary": "Select individually the partner visibility on each company",
-    "version": "10.0.1.0.0",
+    "version": "11.0.1.0.0",
     "license": "AGPL-3",
     "depends": [
         "base_multi_company",
