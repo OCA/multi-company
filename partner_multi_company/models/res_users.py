dddf4a902bc56b93d3009d4ba13ec29b48f52db8
cubells <vicent@vcubells.net>
2018-11-15 10:55:57 +0100

    [MIG] partner_multi_company: Migration to 11.0

--- partner_multi_company/models/res_users.py
+++ partner_multi_company/models/res_users.py
@@ -1,5 +1,4 @@
-# -*- coding: utf-8 -*-
-# © 2015-2016 Pedro M. Baeza <pedro.baeza@tecnativa.com>
+# Copyright 2015-2016 Pedro M. Baeza <pedro.baeza@tecnativa.com>
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html.html
 
 from odoo import api, models
