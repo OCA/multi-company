4f8488fd1f7b73e93ab845937cc0b927d07d5bf4
Mourad EL HADJ MIMOUNE <mourad.elhadj.mimoune@akretion.com>
2019-02-21 08:39:09 +0100

    [FIX] OCA Review

--- purchase_sale_inter_company/__manifest__.py
+++ purchase_sale_inter_company/__manifest__.py
@@ -1,12 +1,12 @@
 # Copyright 2013-Today Odoo SA
-# Copyright 2016 Chafique DELLI @ Akretion
-# Copyright 2018 Tecnativa - Carlos Dauden
+# Copyright 2016-2019 Chafique DELLI @ Akretion
+# Copyright 2018-2019 Tecnativa - Carlos Dauden
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 {
     'name': 'Inter Company Module for Purchase to Sale Order',
     'summary': 'Intercompany PO/SO rules',
-    'version': '11.0.1.0.0',
+    'version': '12.0.1.0.0',
     'category': 'Purchase Management',
     'website': 'http://www.github.com/OCA/multi-company',
     'author': 'Odoo SA, '
@@ -22,8 +22,6 @@
         'account_invoice_inter_company',
     ],
     'data': [
-        'views/res_company_view.xml',
         'views/res_config_view.xml',
-        'views/purchase_order_view.xml',
     ],
 }
--- purchase_sale_inter_company/models/res_config.py
+++ purchase_sale_inter_company/models/res_config.py
@@ -1,6 +1,6 @@
 # Copyright 2013-Today Odoo SA
-# Copyright 2016 Chafique DELLI @ Akretion
-# Copyright 2018 Tecnativa - Carlos Dauden
+# Copyright 2016-2019 Chafique DELLI @ Akretion
+# Copyright 2018-2019 Tecnativa - Carlos Dauden
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 from odoo import fields, models
