ff92e600f382b043f811a02c3df97b7856815539
David Beal <david.beal@akretion.com>
2019-03-19 10:42:12 +0100

    FIX account_invoice_inter_company: switch config related field to readonly False

--- account_invoice_inter_company/models/res_config_settings.py
+++ account_invoice_inter_company/models/res_config_settings.py
@@ -9,6 +9,6 @@ class ResConfigSettings(models.TransientModel):
 
     invoice_auto_validation = fields.Boolean(
         related='company_id.invoice_auto_validation',
-        string='Invoices Auto Validation',
+        string='Invoices Auto Validation', readonly=False,
         help='When an invoice is created by a multi company rule for '
              'this company, it will automatically validate it.')
