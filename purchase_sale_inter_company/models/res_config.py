7abf4960ad7fc2991e5312f1ee321c8629844680
David Beal <david.beal@akretion.com>
2019-03-19 14:53:08 +0100

    IMP purch_sale_inter_cpny: layout settings

--- purchase_sale_inter_company/models/res_config.py
+++ purchase_sale_inter_company/models/res_config.py
@@ -28,7 +28,7 @@ class InterCompanyRulesConfig(models.TransientModel):
     warehouse_id = fields.Many2one(
         comodel_name='stock.warehouse',
         related='company_id.warehouse_id',
-        string='Warehouse For Sale Orders',
+        string='Warehouse for Sale Orders',
         help='Default value to set on Sale Orders that will be created '
         'based on Purchase Orders made to this company.',
         readonly=False,
