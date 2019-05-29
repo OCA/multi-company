768a5e6e8f2ddabfdba9140454d8b209158a873b
Quentin Groulard <quentin.groulard@acsone.eu>
2019-05-29 15:22:47 +0200

    [IMP] company_id domain and default in methods
    In order to ease overriding

--- crm_stage_multi_company/models/crm_stage.py
+++ crm_stage_multi_company/models/crm_stage.py
@@ -8,12 +8,18 @@ class CrmStage(models.Model):
 
     _inherit = "crm.stage"
 
+    def _get_default_company_id(self):
+        return self.env.user.company_id.id
+
+    def _get_company_id_domain(self):
+        return [("id", "child_of", self.env.user.company_id.id)]
+
     company_id = fields.Many2one(
         "res.company",
         string="Company",
         index=True,
         help="Specific company that uses this stage. "
         "Other companies will not be able to see or use this stage.",
-        domain=lambda self: [("id", "child_of", self.env.user.company_id.id)],
-        default=lambda self: self.env.user.company_id.id,
+        domain=lambda self: self._get_company_id_domain(),
+        default=lambda self: self._get_default_company_id(),
     )
