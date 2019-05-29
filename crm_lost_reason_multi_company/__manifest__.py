7cfb9fefba6fe533f700ffb1a2ed9c5d64675ad4
sbejaoui <souheil.bejaoui@acsone.eu>
2019-05-29 14:03:02 +0200

    [IMP] - Add multi company management to crm lost reason

---
+++ crm_lost_reason_multi_company/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2019 ACSONE SA/NV
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Crm Lost Reason Multi Company',
+    'summary': """
+        This module add multi-company management to crm lost reason""",
+    'version': '12.0.1.0.0',
+    'license': 'AGPL-3',
+    'author': 'ACSONE SA/NV,'
+              'Odoo Community Association (OCA)',
+    'website': 'https://github.com/OCA/multi-company',
+    'depends': ['crm'],
+    'data': [
+        'security/crm_lost_reason.xml',
+        'views/crm_lost_reason.xml',
+    ],
+}
