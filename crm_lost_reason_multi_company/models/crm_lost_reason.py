5ecf79d219ce8c582e61fa7b941ed9dc787463c6
sbejaoui <souheil.bejaoui@acsone.eu>
2019-05-29 14:03:02 +0200

    [IMP] - Add multi company management to crm lost reason

---
+++ crm_lost_reason_multi_company/models/crm_lost_reason.py
@@ -0,0 +1,18 @@
+# Copyright 2019 ACSONE SA/NV
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import fields, models
+
+
+class CrmLostReason(models.Model):
+
+    _inherit = 'crm.lost.reason'
+
+    company_id = fields.Many2one(
+        'res.company',
+        'Company',
+        default=lambda self: self.env['res.company']._company_default_get(
+            'crm.lost.reason'
+        ),
+        ondelete='cascade',
+    )
