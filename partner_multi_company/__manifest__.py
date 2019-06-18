2eed9c9e9fc7320270c197ae20c6d2186dad6b69
Kitti U <kittiu@gmail.com>
2019-06-18 17:25:12 +0700

    [12.0][MIG] partner_multi_company

--- partner_multi_company/__manifest__.py
+++ partner_multi_company/__manifest__.py
@@ -5,7 +5,7 @@
 {
     "name": "Partner multi-company",
     "summary": "Select individually the partner visibility on each company",
-    "version": "11.0.1.0.0",
+    "version": "12.0.1.0.0",
     "license": "AGPL-3",
     "depends": [
         "base_multi_company",
