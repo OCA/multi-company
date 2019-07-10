e43e56bc77aabef71f60de204daa590484b7722e
sbejaoui <souheil.bejaoui@acsone.eu>
2019-07-10 13:27:59 +0200

    [ADD] - Add multi-company management to mail templates

---
+++ mail_template_multi_company/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright 2017 ACSONE SA/NV
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Mail Template Multi Company',
+    'description': """
+        Make email template a multi company model""",
+    'version': '12.0.1.0.0',
+    'license': 'AGPL-3',
+    'author': 'ACSONE SA/NV,'
+              'Odoo Community Association (OCA)',
+    'website': 'https://github.com/OCA/multi-company',
+    'depends': ['mail'],
+    'data': ['security/mail_template.xml', 'views/mail_template.xml'],
+}
