c890fee28044b7b3bd1946dcf0aac9c223cb9f0a
Andrea <a.stirpe@onestein.nl>
2019-08-06 12:07:16 +0200

    [12.0][MIG] ir_actions_report_multi_company

--- ir_actions_report_multi_company/models/ir_actions_report.py
+++ ir_actions_report_multi_company/models/ir_actions_report.py
@@ -8,4 +8,4 @@ class IrActionsReport(models.Model):
 
     _inherit = 'ir.actions.report'
 
-    company_id = fields.Many2one('res.company', string='Company')
+    company_id = fields.Many2one('res.company')
