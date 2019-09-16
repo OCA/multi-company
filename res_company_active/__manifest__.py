8ae2eb089a7f670920352a6c1c1be04b59182ff2
Sylvain LE GAL <sylvain.legal@grap.coop>
2019-09-16 18:06:40 +0200

    [PORT] res_company_active from 8.0 to 12.0

--- res_company_active/__manifest__.py
+++ res_company_active/__manifest__.py
@@ -1,4 +1,3 @@
-# coding: utf-8
 # Copyright (C) 2019 - Today: GRAP (http://www.grap.coop)
 # @author: Sylvain LE GAL (https://twitter.com/legalsylvain)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
@@ -6,10 +5,10 @@
 {
     'name': 'Company Active',
     'summary': "Add the 'active' feature on company model",
-    'version': '8.0.1.0.0',
+    'version': '12.0.1.0.0',
     'category': 'Tools',
-    'author': 'GRAP',
-    'website': 'http://www.grap.coop',
+    'author': 'GRAP, Odoo Community Association (OCA)',
+    'website': 'https://github.com/OCA/multi-company',
     'license': 'AGPL-3',
     'depends': [
         'base',
