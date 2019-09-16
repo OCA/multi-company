e403584024faef56e909482d0ecf90f7ef8edfc5
Sylvain LE GAL <sylvain.legal@grap.coop>
2019-09-16 18:06:40 +0200

    [PORT] res_company_active from 8.0 to 12.0

--- res_company_active/models/res_company.py
+++ res_company_active/models/res_company.py
@@ -1,10 +1,9 @@
-# coding: utf-8
 # Copyright (C) 2019 - Today: GRAP (http://www.grap.coop)
 # @author: Sylvain LE GAL (https://twitter.com/legalsylvain)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from openerp import _, api, fields, models
-from openerp.exceptions import ValidationError
+from odoo import _, api, fields, models
+from odoo.exceptions import ValidationError
 
 
 class ResCompany(models.Model):
@@ -12,13 +11,15 @@ class ResCompany(models.Model):
 
     active = fields.Boolean(string='Active', default=True)
 
-    @api.multi
     @api.constrains('active')
     def _check_active(self):
         ResUsers = self.env['res.users']
 
         for company in self:
             if not company.active:
+                if self.env.user.company_id == company:
+                    raise ValidationError(_(
+                        "You can not disable the current company."))
                 # check if it it the current company for some users
                 users = ResUsers.search([('company_id', '=', company.id)])
                 if len(users):
@@ -26,7 +27,7 @@ class ResCompany(models.Model):
                         "You can not disable the company %s because it is the"
                         " current company for the following active users:\n\n"
                         " - %s\n\n"
-                        " Please change the company of there users, or disable"
+                        " Please change the company of these users, or disable"
                         " them") % (
                             company.name,
                             '\n - '.join(users.mapped('name'))))
