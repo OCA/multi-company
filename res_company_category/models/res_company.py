ef61575bf6e683a587f00d447af35480279543b7
Sylvain LE GAL <sylvain.legal@grap.coop>
2019-09-16 18:57:49 +0200

    [PORT] res_company_category from 8.0 to 12.0

--- res_company_category/models/res_company.py
+++ res_company_category/models/res_company.py
@@ -1,9 +1,8 @@
-# coding: utf-8
 # Copyright (C) 2019 - Today: GRAP (http://www.grap.coop)
 # @author: Sylvain LE GAL (https://twitter.com/legalsylvain)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from openerp import fields, models
+from odoo import fields, models
 
 
 class ResCompany(models.Model):
