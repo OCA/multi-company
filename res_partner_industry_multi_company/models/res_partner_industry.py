5f0eb98fc0e456d38c07afc9b3c1d28d8bab8c5b
Quentin Groulard <quentin.groulard@acsone.eu>
2019-10-30 09:36:24 +0100

    [12.0] [ADD] res_partner_industry_multi_company

---
+++ res_partner_industry_multi_company/models/res_partner_industry.py
@@ -0,0 +1,18 @@
+# Copyright 2019 ACSONE SA/NV
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import fields, models
+
+
+class ResPartnerIndustry(models.Model):
+
+    _inherit = 'res.partner.industry'
+
+    company_id = fields.Many2one(
+        'res.company',
+        'Company',
+        default=lambda self: self.env['res.company']._company_default_get(
+            'res.partner.industry'
+        ),
+        ondelete='cascade',
+    )
