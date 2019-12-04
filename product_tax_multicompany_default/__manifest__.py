cec87b46e502bb6fee7ac4699a84ef3f73514bd8
Carlos Dauden <carlos.dauden@tecnativa.com>
2019-12-04 19:37:53 +0100

    [MIG] product_tax_multicompany_default: Migration to 12.0

--- product_tax_multicompany_default/__manifest__.py
+++ product_tax_multicompany_default/__manifest__.py
@@ -1,10 +1,10 @@
-# Copyright 2017 Carlos Dauden - Tecnativa <carlos.dauden@tecnativa.com>
-# Copyright 2018 Vicent Cubells - Tecnativa <vicent.cubells@tecnativa.com>
+# Copyright 2017-2019 Tecnativa - Carlos Dauden
+# Copyright 2018 Tecnativa - Vicent Cubells
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     'name': 'Product Tax Multi Company Default',
-    'version': '11.0.1.0.0',
+    'version': '12.0.1.0.0',
     'category': 'Account',
     'website': 'https://www.tecnativa.com',
     'author': 'Tecnativa, '
