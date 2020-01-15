b61466f25893fb5549361c26bc5e2e5ced408de6
Maxime Chambreuil <mchambreuil@opensourceintegrators.com>
2020-01-15 11:37:58 -0600

    [FIX] account_move_multi_company

--- account_move_multi_company/__manifest__.py
+++ account_move_multi_company/__manifest__.py
@@ -3,21 +3,21 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    'name': 'Account Move Multicompany',
-    'version': '12.0.1.0.0',
-    'category': 'Accounting & Finance',
-    'author': 'Open Source Integrators, Odoo Community Association (OCA)',
-    'website': 'https://github.com/OCA/multi-company',
-    'license': 'AGPL-3',
-    'depends': [
-        'account_invoice_consolidated',
+    "name": "Account Move Multi-Company",
+    "summary": "Allow to transfer amount to other companies",
+    "version": "12.0.1.0.0",
+    "category": "Accounting & Finance",
+    "author": "Open Source Integrators, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/multi-company",
+    "license": "AGPL-3",
+    "depends": [
+        "account_invoice_consolidated",
     ],
-    'data': [
-        'views/account_move_view.xml',
+    "data": [
+        "security/ir_rule.xml",
+        "views/account_move_view.xml",
     ],
-    'installable': True,
-    'development_status': 'Beta',
-    'maintainers': [
-        'max3903',
-    ]
+    "installable": True,
+    "development_status": "Beta",
+    "maintainers": ["max3903"],
 }
