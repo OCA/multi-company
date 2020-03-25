1ac6860600052d51fe23805e3d63ca7f5da6417e
Joan Sisquella <joan.sisquella@forgeflow.com>
2020-03-25 13:41:32 +0100

    [IMP] partner_multi_company: black, isort

--- partner_multi_company/models/res_users.py
+++ partner_multi_company/models/res_users.py
@@ -10,21 +10,21 @@ class ResUsers(models.Model):
     @api.model
     def create(self, vals):
         res = super(ResUsers, self).create(vals)
-        if 'company_ids' in vals:
-            res.partner_id.company_ids = vals['company_ids']
-        if 'company_id' in vals and res.partner_id.company_ids:
-            res.partner_id.company_id = vals['company_id']
+        if "company_ids" in vals:
+            res.partner_id.company_ids = vals["company_ids"]
+        if "company_id" in vals and res.partner_id.company_ids:
+            res.partner_id.company_id = vals["company_id"]
         return res
 
     @api.multi
     def write(self, vals):
         res = super(ResUsers, self).write(vals)
-        if 'company_ids' in vals:
+        if "company_ids" in vals:
             for user in self.sudo():
                 if user.partner_id.company_ids:
-                    user.partner_id.company_ids = vals['company_ids']
-        if 'company_id' in vals:
+                    user.partner_id.company_ids = vals["company_ids"]
+        if "company_id" in vals:
             for user in self.sudo():
                 if user.partner_id.company_ids:
-                    user.partner_id.company_ids = [(4, vals['company_id'])]
+                    user.partner_id.company_ids = [(4, vals["company_id"])]
         return res
