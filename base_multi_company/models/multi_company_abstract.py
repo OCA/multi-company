fb67b984c1b25d0f1fcadb7646d1e01e89b9b599
Joan Sisquella <joan.sisquella@forgeflow.com>
2020-03-25 12:47:56 +0100

    [IMP] base_multi_company: black, isort

--- base_multi_company/models/multi_company_abstract.py
+++ base_multi_company/models/multi_company_abstract.py
@@ -6,29 +6,30 @@ from odoo import api, fields, models
 
 class MultiCompanyAbstract(models.AbstractModel):
 
-    _name = 'multi.company.abstract'
-    _description = 'Multi-Company Abstract'
+    _name = "multi.company.abstract"
+    _description = "Multi-Company Abstract"
 
     company_id = fields.Many2one(
-        string='Company',
-        comodel_name='res.company',
-        compute='_compute_company_id',
-        inverse='_inverse_company_id',
-        search='_search_company_id',
+        string="Company",
+        comodel_name="res.company",
+        compute="_compute_company_id",
+        inverse="_inverse_company_id",
+        search="_search_company_id",
     )
     company_ids = fields.Many2many(
-        string='Companies',
-        comodel_name='res.company.assignment',
+        string="Companies",
+        comodel_name="res.company.assignment",
         auto_join=True,
         default=lambda self: self._default_company_ids(),
     )
     visible_for_all_companies = fields.Boolean(
-        compute='_compute_visible_for_all_companies',
+        compute="_compute_visible_for_all_companies",
         store=True,
         index=True,
-        default=True)
+        default=True,
+    )
 
-    @api.depends('company_ids')
+    @api.depends("company_ids")
     @api.multi
     def _compute_visible_for_all_companies(self):
         for rec in self:
@@ -36,11 +37,9 @@ class MultiCompanyAbstract(models.AbstractModel):
                 rec.visible_for_all_companies = True
 
     def _default_company_ids(self):
-        return self.browse(
-            self.env['res.company']._company_default_get(self._name).ids
-        )
+        return self.browse(self.env["res.company"]._company_default_get(self._name).ids)
 
-    @api.depends('company_ids')
+    @api.depends("company_ids")
     def _compute_company_id(self):
         user_company = self.env.user.company_id
         for record in self:
@@ -61,8 +60,8 @@ class MultiCompanyAbstract(models.AbstractModel):
             else:
                 # Empty the list of allowed companies (so it means all
                 # companies are allowed) as it's the equivalent
-                record.company_ids = [(5, )]
+                record.company_ids = [(5,)]
 
     @api.model
     def _search_company_id(self, operator, value):
-        return [('company_ids', operator, value)]
+        return [("company_ids", operator, value)]
