16eb0ee3e46a76fc53e2cd75feb577da4c136ab8
Joan Sisquella <joan.sisquella@forgeflow.com>
2020-03-25 12:47:56 +0100

    [IMP] base_multi_company: black, isort

--- base_multi_company/hooks.py
+++ base_multi_company/hooks.py
@@ -2,22 +2,23 @@
 # Copyright 2017 LasLabs Inc.
 # License LGPL-3 - See http://www.gnu.org/licenses/lgpl-3.0.html
 
-from odoo import api, SUPERUSER_ID
-
+from odoo import SUPERUSER_ID, api
 
 __all__ = [
-    'create_company_assignment_view',
-    'post_init_hook',
-    'uninstall_hook',
+    "create_company_assignment_view",
+    "post_init_hook",
+    "uninstall_hook",
 ]
 
 
 def create_company_assignment_view(cr):
-    cr.execute("""
+    cr.execute(
+        """
         CREATE OR REPLACE VIEW res_company_assignment
             AS SELECT id, name, parent_id
             FROM res_company;
-    """)
+    """
+    )
 
 
 def set_security_rule(env, rule_ref):
@@ -29,13 +30,15 @@ def set_security_rule(env, rule_ref):
     rule = env.ref(rule_ref)
     if not rule:  # safeguard if it's deleted
         return
-    rule.write({
-        'active': True,
-        'domain_force': (
-            "['|', ('company_ids', 'in', user.company_id.ids),"
-            " ('visible_for_all_companies', '=', True)]"
-        ),
-    })
+    rule.write(
+        {
+            "active": True,
+            "domain_force": (
+                "['|', ('company_ids', 'in', user.company_id.ids),"
+                " ('visible_for_all_companies', '=', True)]"
+            ),
+        }
+    )
 
 
 def post_init_hook(cr, rule_ref, model_name):
@@ -53,14 +56,19 @@ def post_init_hook(cr, rule_ref, model_name):
         set_security_rule(env, rule_ref)
         # Copy company values
         model = env[model_name]
-        table_name = model._fields['company_ids'].relation
-        column1 = model._fields['company_ids'].column1
-        column2 = model._fields['company_ids'].column2
+        table_name = model._fields["company_ids"].relation
+        column1 = model._fields["company_ids"].column1
+        column2 = model._fields["company_ids"].column2
         SQL = """
             INSERT INTO %s
             (%s, %s)
             SELECT id, company_id FROM %s WHERE company_id IS NOT NULL
-        """ % (table_name, column1, column2, model._table)
+        """ % (
+            table_name,
+            column1,
+            column2,
+            model._table,
+        )
         env.cr.execute(SQL)
 
 
@@ -76,10 +84,12 @@ def uninstall_hook(cr, rule_ref):
         env = api.Environment(cr, SUPERUSER_ID, {})
         # Change access rule
         rule = env.ref(rule_ref)
-        rule.write({
-            'active': False,
-            'domain_force': (
-                " ['|', ('company_id', '=', user.company_id.id),"
-                " ('company_id', '=', False)]"
-            ),
-        })
+        rule.write(
+            {
+                "active": False,
+                "domain_force": (
+                    " ['|', ('company_id', '=', user.company_id.id),"
+                    " ('company_id', '=', False)]"
+                ),
+            }
+        )
