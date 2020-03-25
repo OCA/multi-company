9c2f669186ce16c0448c2c4f41f1a8d4d376cca0
Joan Sisquella <joan.sisquella@forgeflow.com>
2020-03-25 12:47:56 +0100

    [IMP] base_multi_company: black, isort

--- base_multi_company/__manifest__.py
+++ base_multi_company/__manifest__.py
@@ -2,19 +2,17 @@
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
 
 {
-    'name': 'Multi Company Base',
-    'summary': 'Provides a base for adding multi-company support to models.',
-    'version': '12.0.1.0.1',
-    'author': "LasLabs, Tecnativa, Odoo Community Association (OCA)",
-    'category': 'base',
-    'website': 'https://github.com/OCA/multi-company',
-    'license': 'LGPL-3',
-    'installable': True,
-    'application': False,
-    'pre_init_hook': 'create_company_assignment_view',
-    'data': [
-        'security/ir.model.access.csv',
-    ],
-    'development_status': 'Production/Stable',
-    'maintainers': ['pedrobaeza'],
+    "name": "Multi Company Base",
+    "summary": "Provides a base for adding multi-company support to models.",
+    "version": "12.0.1.0.1",
+    "author": "LasLabs, Tecnativa, Odoo Community Association (OCA)",
+    "category": "base",
+    "website": "https://github.com/OCA/multi-company",
+    "license": "LGPL-3",
+    "installable": True,
+    "application": False,
+    "pre_init_hook": "create_company_assignment_view",
+    "data": ["security/ir.model.access.csv",],
+    "development_status": "Production/Stable",
+    "maintainers": ["pedrobaeza"],
 }
