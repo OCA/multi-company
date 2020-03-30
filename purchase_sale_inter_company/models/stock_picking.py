ed32fd3ab67ed610bc924f324451145706080093
Adrià Gil Sorribes <adria.gil@forgeflow.com>
2020-03-30 16:03:10 +0200

    [13.0][MIG] purchase_sale_inter_company
    rename intercompany_user_id to intercompany_sale_user_id for compatibility with account_invoice_inter_company
    
    [IMP] res_config_settings view

--- purchase_sale_inter_company/models/stock_picking.py
+++ purchase_sale_inter_company/models/stock_picking.py
@@ -2,7 +2,7 @@
 # Copyright 2018 Tecnativa - Pedro M. Baeza
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
-from odoo import _, api, fields, models
+from odoo import _, fields, models
 from odoo.exceptions import UserError
 
 
@@ -11,8 +11,7 @@ class StockPicking(models.Model):
 
     intercompany_picking_id = fields.Many2one(comodel_name="stock.picking")
 
-    @api.multi
-    def do_transfer(self):
+    def action_done(self):
         # Only DropShip pickings
         po_picks = self.browse()
         for pick in self.filtered(
@@ -24,7 +23,8 @@ class StockPicking(models.Model):
             purchase.picking_ids.write({"intercompany_picking_id": pick.id})
             for move_line in pick.move_line_ids:
                 qty_done = move_line.qty_done
-                po_move_lines = move_line.move_id.sale_line_id.auto_purchase_line_id.move_ids.mapped(
+                sale_line_id = move_line.move_id.sale_line_id
+                po_move_lines = sale_line_id.auto_purchase_line_id.move_ids.mapped(
                     "move_line_ids"
                 )
                 for po_move_line in po_move_lines:
@@ -48,4 +48,4 @@ class StockPicking(models.Model):
         # Transfer dropship pickings
         for po_pick in po_picks.sudo():
             po_pick.with_context(force_company=po_pick.company_id.id,).action_done()
-        return super(StockPicking, self).do_transfer()
+        return super(StockPicking, self).action_done()
