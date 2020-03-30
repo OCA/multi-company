83cb6f17f68011efb4859752d4313986dc4603b6
Adrià Gil Sorribes <adria.gil@forgeflow.com>
2020-03-30 10:46:13 +0200

    [IMP] purchase_sale_inter_company: black, isort

--- purchase_sale_inter_company/models/purchase_order.py
+++ purchase_sale_inter_company/models/purchase_order.py
@@ -8,7 +8,7 @@ from odoo.exceptions import UserError
 
 
 class PurchaseOrder(models.Model):
-    _inherit = 'purchase.order'
+    _inherit = "purchase.order"
 
     @api.multi
     def button_approve(self, force=False):
@@ -27,26 +27,29 @@ class PurchaseOrder(models.Model):
     @api.multi
     def _get_user_domain(self, dest_company):
         self.ensure_one()
-        group_purchase_user = self.env.ref('purchase.group_purchase_user')
+        group_purchase_user = self.env.ref("purchase.group_purchase_user")
         return [
-            ('id', '!=', 1),
-            ('company_id', '=', dest_company.id),
-            ('id', 'in', group_purchase_user.users.ids),
+            ("id", "!=", 1),
+            ("company_id", "=", dest_company.id),
+            ("id", "in", group_purchase_user.users.ids),
         ]
 
     @api.multi
     def _check_intercompany_product(self, dest_company):
         domain = self._get_user_domain(dest_company)
-        dest_user = self.env['res.users'].search(domain, limit=1)
+        dest_user = self.env["res.users"].search(domain, limit=1)
         if dest_user:
             for purchase_line in self.order_line:
                 try:
-                    purchase_line.product_id.sudo(dest_user).read(
-                        ['default_code'])
+                    purchase_line.product_id.sudo(dest_user).read(["default_code"])
                 except Exception:
-                    raise UserError(_(
-                        "You cannot create SO from PO because product '%s' "
-                        "is not intercompany") % purchase_line.product_id.name)
+                    raise UserError(
+                        _(
+                            "You cannot create SO from PO because product '%s' "
+                            "is not intercompany"
+                        )
+                        % purchase_line.product_id.name
+                    )
 
     @api.multi
     def _inter_company_create_sale_order(self, dest_company):
@@ -70,21 +73,29 @@ class PurchaseOrder(models.Model):
         company_partner = self.company_id.partner_id
         # check pricelist currency should be same with PO/SO document
         if self.currency_id.id != (
-                company_partner.property_product_pricelist.currency_id.id):
-            raise UserError(_(
-                'You cannot create SO from PO because '
-                'sale price list currency is different than '
-                'purchase price list currency.'))
+            company_partner.property_product_pricelist.currency_id.id
+        ):
+            raise UserError(
+                _(
+                    "You cannot create SO from PO because "
+                    "sale price list currency is different than "
+                    "purchase price list currency."
+                )
+            )
         # create the SO and generate its lines from the PO lines
         sale_order_data = self._prepare_sale_order_data(
-            self.name, company_partner, dest_company, self.dest_address_id)
-        sale_order = self.env['sale.order'].sudo(
-            intercompany_user.id).create(sale_order_data)
+            self.name, company_partner, dest_company, self.dest_address_id
+        )
+        sale_order = (
+            self.env["sale.order"].sudo(intercompany_user.id).create(sale_order_data)
+        )
         for purchase_line in self.order_line:
             sale_line_data = self._prepare_sale_order_line_data(
-                purchase_line, dest_company, sale_order)
-            self.env['sale.order.line'].sudo(
-                intercompany_user.id).create(sale_line_data)
+                purchase_line, dest_company, sale_order
+            )
+            self.env["sale.order.line"].sudo(intercompany_user.id).create(
+                sale_line_data
+            )
         # write supplier reference field on PO
         if not self.partner_ref:
             self.partner_ref = sale_order.name
@@ -93,8 +104,9 @@ class PurchaseOrder(models.Model):
             sale_order.sudo(intercompany_user.id).action_confirm()
 
     @api.multi
-    def _prepare_sale_order_data(self, name, partner, dest_company,
-                                 direct_delivery_address):
+    def _prepare_sale_order_data(
+        self, name, partner, dest_company, direct_delivery_address
+    ):
         """ Generate the Sale Order values from the PO
             :param name : the origin client reference
             :rtype name : string
@@ -107,33 +119,38 @@ class PurchaseOrder(models.Model):
         """
         self.ensure_one()
         delivery_address = (
-            direct_delivery_address or
-            self.picking_type_id.warehouse_id.partner_id or False)
-        new_order = self.env['sale.order'].new({
-            'company_id': dest_company.id,
-            'client_order_ref': name,
-            'partner_id': partner.id,
-            'date_order': self.date_order,
-            'auto_purchase_order_id': self.id,
-        })
-        for onchange_method in new_order._onchange_methods['partner_id']:
+            direct_delivery_address
+            or self.picking_type_id.warehouse_id.partner_id
+            or False
+        )
+        new_order = self.env["sale.order"].new(
+            {
+                "company_id": dest_company.id,
+                "client_order_ref": name,
+                "partner_id": partner.id,
+                "date_order": self.date_order,
+                "auto_purchase_order_id": self.id,
+            }
+        )
+        for onchange_method in new_order._onchange_methods["partner_id"]:
             onchange_method(new_order)
         new_order.user_id = False
         if delivery_address:
             new_order.partner_shipping_id = delivery_address
         if self.notes:
             new_order.note = self.notes
-        if 'warehouse_id' in new_order:
+        if "warehouse_id" in new_order:
             new_order.warehouse_id = (
-                dest_company.warehouse_id.company_id == dest_company and
-                dest_company.warehouse_id or False)
-        if 'requested_date' in new_order:
+                dest_company.warehouse_id.company_id == dest_company
+                and dest_company.warehouse_id
+                or False
+            )
+        if "requested_date" in new_order:
             new_order.requested_date = self.date_planned
         return new_order._convert_to_write(new_order._cache)
 
     @api.model
-    def _prepare_sale_order_line_data(
-            self, purchase_line, dest_company, sale_order):
+    def _prepare_sale_order_line_data(self, purchase_line, dest_company, sale_order):
         """ Generate the Sale Order Line values from the PO line
             :param purchase_line : the origin Purchase Order Line
             :rtype purchase_line : purchase.order.line record
@@ -141,27 +158,31 @@ class PurchaseOrder(models.Model):
             :rtype dest_company : res.company record
             :param sale_order : the Sale Order
         """
-        new_line = self.env['sale.order.line'].new({
-            'order_id': sale_order.id,
-            'product_id': purchase_line.product_id.id,
-            'product_uom': purchase_line.product_uom.id,
-            'product_uom_qty': purchase_line.product_qty,
-            'auto_purchase_line_id': purchase_line.id,
-        })
-        for onchange_method in new_line._onchange_methods['product_id']:
+        new_line = self.env["sale.order.line"].new(
+            {
+                "order_id": sale_order.id,
+                "product_id": purchase_line.product_id.id,
+                "product_uom": purchase_line.product_uom.id,
+                "product_uom_qty": purchase_line.product_qty,
+                "auto_purchase_line_id": purchase_line.id,
+            }
+        )
+        for onchange_method in new_line._onchange_methods["product_id"]:
             onchange_method(new_line)
         return new_line._convert_to_write(new_line._cache)
 
     @api.multi
     def button_cancel(self):
-        sale_orders = self.env['sale.order'].sudo().search([
-            ('auto_purchase_order_id', 'in', self.ids)])
+        sale_orders = (
+            self.env["sale.order"]
+            .sudo()
+            .search([("auto_purchase_order_id", "in", self.ids)])
+        )
         for so in sale_orders:
-            if so.state not in ['draft', 'sent', 'cancel']:
-                raise UserError(_("You can't cancel an order that is %s")
-                                % so.state)
+            if so.state not in ["draft", "sent", "cancel"]:
+                raise UserError(_("You can't cancel an order that is %s") % so.state)
         sale_orders.action_cancel()
-        self.write({
-            'partner_ref': False,
-        })
+        self.write(
+            {"partner_ref": False,}
+        )
         return super().button_cancel()
--- purchase_sale_inter_company/models/res_config.py
+++ purchase_sale_inter_company/models/res_config.py
@@ -8,34 +8,34 @@ from odoo import fields, models
 
 class InterCompanyRulesConfig(models.TransientModel):
 
-    _inherit = 'res.config.settings'
+    _inherit = "res.config.settings"
 
     so_from_po = fields.Boolean(
-        related='company_id.so_from_po',
+        related="company_id.so_from_po",
         string="Create Sale Orders when buying to this company",
-        help='Generate a Sale Order when a Purchase Order with this company '
-        'as supplier is created.\n The intercompany user must at least be '
-        'Sale User.',
+        help="Generate a Sale Order when a Purchase Order with this company "
+        "as supplier is created.\n The intercompany user must at least be "
+        "Sale User.",
         readonly=False,
     )
     sale_auto_validation = fields.Boolean(
-        related='company_id.sale_auto_validation',
-        string='Sale Orders Auto Validation',
-        help='When a Sale Order is created by a multi company rule for '
-             'this company, it will automatically validate it.',
+        related="company_id.sale_auto_validation",
+        string="Sale Orders Auto Validation",
+        help="When a Sale Order is created by a multi company rule for "
+        "this company, it will automatically validate it.",
         readonly=False,
     )
     warehouse_id = fields.Many2one(
-        comodel_name='stock.warehouse',
-        related='company_id.warehouse_id',
-        string='Warehouse for Sale Orders',
-        help='Default value to set on Sale Orders that will be created '
-        'based on Purchase Orders made to this company.',
+        comodel_name="stock.warehouse",
+        related="company_id.warehouse_id",
+        string="Warehouse for Sale Orders",
+        help="Default value to set on Sale Orders that will be created "
+        "based on Purchase Orders made to this company.",
         readonly=False,
     )
     intercompany_user_id = fields.Many2one(
-        comodel_name='res.users',
-        related='company_id.intercompany_user_id',
-        string='Intercompany User',
+        comodel_name="res.users",
+        related="company_id.intercompany_user_id",
+        string="Intercompany User",
         readonly=False,
     )
