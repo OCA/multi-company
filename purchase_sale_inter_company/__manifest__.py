a09a1f533e9a0754e95b87a841848f271f6711e7
Adrià Gil Sorribes <adria.gil@forgeflow.com>
2020-03-30 10:46:13 +0200

    [IMP] purchase_sale_inter_company: black, isort

--- purchase_sale_inter_company/__manifest__.py
+++ purchase_sale_inter_company/__manifest__.py
@@ -4,24 +4,14 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 {
-    'name': 'Inter Company Module for Purchase to Sale Order',
-    'summary': 'Intercompany PO/SO rules',
-    'version': '12.0.1.0.0',
-    'category': 'Purchase Management',
-    'website': 'http://www.github.com/OCA/multi-company',
-    'author': 'Odoo SA, '
-              'Akretion, '
-              'Tecnativa, '
-              'Odoo Community Association (OCA)',
-    'license': 'AGPL-3',
-    'installable': True,
-    'depends': [
-        'sale',
-        'purchase',
-        'stock',
-        'account_invoice_inter_company',
-    ],
-    'data': [
-        'views/res_config_view.xml',
-    ],
+    "name": "Inter Company Module for Purchase to Sale Order",
+    "summary": "Intercompany PO/SO rules",
+    "version": "12.0.1.0.0",
+    "category": "Purchase Management",
+    "website": "http://www.github.com/OCA/multi-company",
+    "author": "Odoo SA, " "Akretion, " "Tecnativa, " "Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": ["sale", "purchase", "stock", "account_invoice_inter_company",],
+    "data": ["views/res_config_view.xml",],
 }
