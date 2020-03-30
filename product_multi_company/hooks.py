f6907bd915190f1ae36875a4ce8a7a1d143d2fa0
Joan Sisquella <joan.sisquella@forgeflow.com>
2020-03-30 12:45:48 +0200

    [IMP] product_multi_company: black, isort, prettier

--- product_multi_company/hooks.py
+++ product_multi_company/hooks.py
@@ -8,19 +8,16 @@ _logger = logging.getLogger(__name__)
 try:
     from odoo.addons.base_multi_company import hooks
 except ImportError:
-    _logger.info('Cannot find `base_multi_company` module in addons path.')
+    _logger.info("Cannot find `base_multi_company` module in addons path.")
 
 
 def post_init_hook(cr, registry):
     hooks.post_init_hook(
-        cr,
-        'product.product_comp_rule',
-        'product.template',
+        cr, "product.product_comp_rule", "product.template",
     )
 
 
 def uninstall_hook(cr, registry):
     hooks.uninstall_hook(
-        cr,
-        'product.product_comp_rule',
+        cr, "product.product_comp_rule",
     )
