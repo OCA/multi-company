c8a7c6221dd3275cdf0d0b490f50849565cb584c
Valentin Vinagre Urteaga <valentin.vinagre@sygel.es>
2020-05-11 13:22:34 +0200

    [IMP] mail_multicompany: black, isort, prettier

--- mail_multicompany/models/ir_mail_server.py
+++ mail_multicompany/models/ir_mail_server.py
@@ -1,10 +1,9 @@
-# -*- coding: utf-8 -*-
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
-from odoo import models, fields
+from odoo import fields, models
 
 
 class IrMailServer(models.Model):
 
     _inherit = "ir.mail_server"
 
-    company_id = fields.Many2one('res.company', 'Company')
+    company_id = fields.Many2one("res.company", "Company")
