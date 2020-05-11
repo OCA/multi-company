44dfe675c02d3717bba6c83421b4524339f026fe
Valentin Vinagre Urteaga <valentin.vinagre@sygel.es>
2020-05-11 13:27:40 +0200

    [ADD+MIG] mail_multicompany: ADD+MIG to 13.0

--- mail_multicompany/models/mail_message.py
+++ mail_multicompany/models/mail_message.py
@@ -1,4 +1,5 @@
-# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
 from odoo import api, fields, models
 
 
@@ -8,30 +9,24 @@ class MailMessage(models.Model):
 
     company_id = fields.Many2one("res.company", "Company")
 
-    @api.model
-    def create(self, vals):
-        if not vals.get("company_id"):
-            if vals.get("author_id", False):
-                author_user = self.env["res.users"].search(
-                    [("partner_id", "=", vals["author_id"])]
+    @api.model_create_multi
+    def create(self, values_list):
+        for vals in values_list:
+            if vals.get("model") and vals.get("res_id"):
+                current_object = self.env[vals["model"]].browse(vals["res_id"])
+                if hasattr(current_object, "company_id") and current_object.company_id:
+                    vals["company_id"] = current_object.company_id.id
+            if not vals.get("company_id"):
+                vals["company_id"] = self.env.company.id
+            if not vals.get("mail_server_id"):
+                vals["mail_server_id"] = (
+                    self.sudo()
+                    .env["ir.mail_server"]
+                    .search(
+                        [("company_id", "=", vals.get("company_id", False))],
+                        order="sequence",
+                        limit=1,
+                    )
+                    .id
                 )
-                company_id = author_user.company_id.id
-            else:
-                company_id = self.env.user.company_id.id
-            vals["company_id"] = company_id
-        if vals.get("model") and vals.get("res_id"):
-            current_object = self.env[vals["model"]].browse(vals["res_id"])
-            if hasattr(current_object, "company_id"):
-                vals["company_id"] = current_object.company_id.id
-        if not vals.get("mail_server_id"):
-            vals["mail_server_id"] = (
-                self.sudo()
-                .env["ir.mail_server"]
-                .search(
-                    [("company_id", "=", vals.get("company_id", False))],
-                    order="sequence",
-                    limit=1,
-                )
-                .id
-            )
         return super(MailMessage, self).create(vals)
