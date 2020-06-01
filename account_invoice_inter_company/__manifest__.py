d64911c05c5643a2c94fb9380b5c3e65c47226ba
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2020-06-01 19:03:16 +0200

    [FIX] account_invoice_inter_company: Handle invoices in multi-line
    Do creation in batch for increasing performance and avoid code to fail in case of
    existing invoice line.

--- account_invoice_inter_company/__manifest__.py
+++ account_invoice_inter_company/__manifest__.py
@@ -1,5 +1,7 @@
 # Copyright 2013-2014 Odoo SA
 # Copyright 2015-2017 Chafique Delli <chafique.delli@akretion.com>
+# Copyright 2020 Tecnativa - David Vidal
+# Copyright 2020 Tecnativa - Pedro M. Baeza
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 {
