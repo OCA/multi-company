19626f1b8c757d530d257c75066492a26f4301f4
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2020-06-01 11:58:45 +0200

    [IMP] account_invoice_inter_company: Move analytic info between invoices
    If suitable (being non company specific data), analytic information (account and
    tags) is moved from source to destination invoice.

--- account_invoice_inter_company/tests/test_inter_company_invoice.py
+++ account_invoice_inter_company/tests/test_inter_company_invoice.py
@@ -33,7 +33,8 @@ class TestAccountInvoiceInterCompanyBase(SavepointCase):
         cls.company_a = cls.env.ref("account_invoice_inter_company.company_a")
         cls.company_b = cls.env.ref("account_invoice_inter_company.company_b")
         cls.company_a.invoice_auto_validation = True
-        cls.product_a = cls.invoice_company_a.invoice_line_ids.product_id
+        cls.invoice_line_a = cls.invoice_company_a.invoice_line_ids[0]
+        cls.product_a = cls.invoice_line_a.product_id
         cls.product_a.with_context(
             force_company=cls.company_b.id
         ).property_account_expense_id = cls.env.ref(
@@ -72,6 +73,19 @@ class TestAccountInvoiceInterCompany(TestAccountInvoiceInterCompanyBase):
         self.product_a.with_context(
             force_company=self.user_company_b.id
         ).supplier_taxes_id = False
+        # Put some analytic data for checking its propagation
+        analytic_account = self.env["account.analytic.account"].create(
+            {"name": "Test analytic account", "company_id": False}
+        )
+        analytic_tag = self.env["account.analytic.tag"].create(
+            {"name": "Test analytic tag", "company_id": False}
+        )
+        self.invoice_line_a.analytic_account_id = analytic_account.id
+        self.invoice_line_a.analytic_tag_ids = [(4, analytic_tag.id)]
+        # Give user A permission to analytic
+        self.user_company_a.groups_id = [
+            (4, self.env.ref("analytic.group_analytic_accounting").id)
+        ]
         # Confirm the invoice of company A
         self.invoice_company_a.with_user(self.user_company_a.id).action_post()
         # Check destination invoice created in company B
@@ -91,9 +105,16 @@ class TestAccountInvoiceInterCompany(TestAccountInvoiceInterCompanyBase):
             len(invoices[0].invoice_line_ids),
             len(self.invoice_company_a.invoice_line_ids),
         )
+        invoice_line = invoices[0].invoice_line_ids[0]
         self.assertEquals(
-            invoices[0].invoice_line_ids[0].product_id,
+            invoice_line.product_id,
             self.invoice_company_a.invoice_line_ids[0].product_id,
+        )
+        self.assertEquals(
+            invoice_line.analytic_account_id, self.invoice_line_a.analytic_account_id
+        )
+        self.assertEquals(
+            invoice_line.analytic_tag_ids, self.invoice_line_a.analytic_tag_ids
         )
         # Cancel the invoice of company A
         invoice_origin = ("%s - Canceled Invoice: %s") % (
