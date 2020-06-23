00b9ff27e1d01beddc7f15695b24f158458cbe81
AaronHForgeFlow <ahenriquez@eficent.com>
2020-06-23 15:02:28 +0200

    [MIG]product_multi_company: Migration to 13.0 (continue)

--- product_multi_company/__manifest__.py
+++ product_multi_company/__manifest__.py
@@ -9,7 +9,7 @@
     "category": "Product Management",
     "version": "13.0.1.0.0",
     "license": "AGPL-3",
-    "depends": ["base_multi_company", "product"],
+    "depends": ["base_multi_company", "product", "stock"],
     "data": ["views/product_template_view.xml"],
     "post_init_hook": "post_init_hook",
     "uninstall_hook": "uninstall_hook",
--- product_multi_company/models/__init__.py
+++ product_multi_company/models/__init__.py
@@ -2,3 +2,4 @@
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
 from . import product_template
+from . import product_category
