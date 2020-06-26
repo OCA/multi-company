f1222ac527963fceae5813266942b23fadd43f32
Kiril Vangelovski <kiril@lambda-is.com>
2020-06-26 19:57:38 +0200

    [IMP] res_company_code: black, isort, prettier

--- res_company_code/models/ir_http.py
+++ res_company_code/models/ir_http.py
@@ -7,19 +7,22 @@ from odoo.http import request
 
 
 class Http(models.AbstractModel):
-    _inherit = 'ir.http'
+    _inherit = "ir.http"
 
     def session_info(self):
 
         result = super().session_info()
         user = request.env.user
-        display_switch_company_menu = user.has_group(
-            'base.group_multi_company') and len(user.company_ids) > 1
+        display_switch_company_menu = (
+            user.has_group("base.group_multi_company") and len(user.company_ids) > 1
+        )
 
         # Replace company name by company complete name in the session
         # The values are used in the switch_company_menu widget (web module)
         result["user_companies"]["current_company"] = (
-            user.company_id.id, user.company_id.complete_name)
+            user.company_id.id,
+            user.company_id.complete_name,
+        )
         if display_switch_company_menu:
             result["user_companies"]["allowed_companies"] = [
                 (comp.id, comp.complete_name) for comp in user.company_ids
