130541c8503d57f8cedea436478a840973512b13
Kiril Vangelovski <kiril@lambda-is.com>
2020-06-26 19:57:38 +0200

    [IMP] res_company_code: black, isort, prettier

--- res_company_code/models/res_company.py
+++ res_company_code/models/res_company.py
@@ -6,23 +6,23 @@ from odoo import api, fields, models
 
 
 class ResCompany(models.Model):
-    _inherit = 'res.company'
-    _order = 'code, name'
+    _inherit = "res.company"
+    _order = "code, name"
 
-    code = fields.Char(string='Code')
+    code = fields.Char(string="Code")
 
     complete_name = fields.Char(
-        string='Complete Name', compute='_compute_complete_name', store=True)
+        string="Complete Name", compute="_compute_complete_name", store=True
+    )
 
     _sql_constraints = [
-        ('code_uniq', 'unique (code)', 'The company code must be unique !')
+        ("code_uniq", "unique (code)", "The company code must be unique !")
     ]
 
-    @api.depends('code', 'name')
+    @api.depends("code", "name")
     def _compute_complete_name(self):
         for company in self:
             if not company.code:
                 company.complete_name = company.name
             else:
-                company.complete_name = "%s - %s" % (
-                    company.code, company.name)
+                company.complete_name = "{} - {}".format(company.code, company.name)
