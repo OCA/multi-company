03cba75b347349ba43f20cbf170cd61ef539ac95
Kiril Vangelovski <kiril@lambda-is.com>
2020-06-26 20:51:21 +0200

    [MIG] res_company_code: Migration to v13.0

--- res_company_code/__manifest__.py
+++ res_company_code/__manifest__.py
@@ -5,14 +5,14 @@
 {
     "name": "Company Code",
     "summary": "Add 'code' field on company model",
-    "version": "12.0.1.0.0",
+    "version": "13.0.1.0.0",
     "category": "Tools",
     "author": "GRAP, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/multi-company",
     "license": "AGPL-3",
-    "depends": ["web",],
-    "data": ["views/view_res_company.xml",],
-    "demo": ["demo/res_groups.xml", "demo/res_company.xml",],
+    "depends": ["web"],
+    "data": ["views/view_res_company.xml"],
+    "demo": ["demo/res_groups.xml", "demo/res_company.xml"],
     "images": [
         "static/description/res_company_form.png",
         "static/description/res_company_tree.png",
