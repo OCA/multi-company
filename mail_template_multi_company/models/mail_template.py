89ecdd82837749cbf4a56a705a0c94f747fd2c05
Andrea <a.stirpe@onestein.nl>
2020-07-10 16:06:45 +0200

    pre-commit

--- mail_template_multi_company/models/mail_template.py
+++ mail_template_multi_company/models/mail_template.py
@@ -6,13 +6,13 @@ from odoo import fields, models
 
 class MailTemplate(models.Model):
 
-    _inherit = 'mail.template'
+    _inherit = "mail.template"
 
     company_id = fields.Many2one(
-        'res.company',
-        'Company',
-        default=lambda self: self.env['res.company']._company_default_get(
-            'mail.template'
+        "res.company",
+        "Company",
+        default=lambda self: self.env["res.company"]._company_default_get(
+            "mail.template"
         ),
         ondelete="set null",
     )
