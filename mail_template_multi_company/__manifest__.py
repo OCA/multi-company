bfa8604f6c64b3b3daff1e68f372733808ef422a
François Honoré <francois.honore@acsone.eu>
2020-07-27 11:35:52 +0200

    [10.0][ADD] Backport of module mail_template_multi_company (from 12.0)

--- mail_template_multi_company/__manifest__.py
+++ mail_template_multi_company/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     'name': 'Mail Template Multi Company',
-    'version': '12.0.1.0.1',
+    'version': '10.0.1.0.1',
     'license': 'AGPL-3',
     'author': 'ACSONE SA/NV,'
               'Odoo Community Association (OCA)',
