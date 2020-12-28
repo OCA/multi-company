31f4da082c86dcf68c54877b1bb54f09fadaac8b
Kevin Graveman <k.graveman@onestein.nl>
2020-12-28 21:40:14 +0100

    [13.0][FIX] Ensure product_template.company_ids is always a superset of product_template.product_variant_ids.company_ids

--- product_multi_company/models/product_template.py
+++ product_multi_company/models/product_template.py
@@ -1,10 +1,36 @@
 # Copyright 2015-2016 Pedro M. Baeza <pedro.baeza@tecnativa.com>
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
-from odoo import models
+from odoo import api, fields, models
 
 
 class ProductTemplate(models.Model):
     _inherit = ["multi.company.abstract", "product.template"]
     _name = "product.template"
     _description = "Product Template (Multi-Company)"
+
+    company_ids = fields.Many2many(
+        compute="_compute_company_ids", inverse="_inverse_company_ids", store=True,
+    )
+
+    @api.depends("product_variant_ids.company_ids")
+    def _compute_company_ids(self):
+        """Set of all companies from all variants plus the template's own companies."""
+        companies_set = set()
+        variants_companies = [
+            variant.company_ids.ids for variant in self.product_variant_ids
+        ]
+        for companies in variants_companies:
+            companies_set.update(companies)
+        self.company_ids += self.env["res.company"].browse(companies_set)
+
+    def _inverse_company_ids(self):
+        """Remove companies from this template's variants which are not assigned "
+        "to the template."""
+        for variant in self.product_variant_ids:
+            remaining_companies = list(
+                set(variant.company_ids.ids).intersection(self.company_ids.ids)
+            )
+            variant.with_context(company_inverse=True).company_ids = [
+                (6, 0, remaining_companies)
+            ]
