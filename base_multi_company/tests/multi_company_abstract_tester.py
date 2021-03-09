cef5d32ed74bac116a3157d6fd96f8a4bdc164bd
Denis Roussel <denis.roussel@acsone.eu>
2021-03-09 18:57:06 +0100

    [14.0][MIG] base_multi_company
    Use odoo-test-helper to test Abstract class

--- base_multi_company/tests/multi_company_abstract_tester.py
+++ base_multi_company/tests/multi_company_abstract_tester.py
@@ -7,5 +7,6 @@ from odoo import fields, models
 class MultiCompanyAbstractTester(models.Model):
     _name = "multi.company.abstract.tester"
     _inherit = "multi.company.abstract"
+    _description = "Multi Company Abstract Tester"
 
     name = fields.Char()
