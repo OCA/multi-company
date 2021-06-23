e59d1ead5678dab1d5c1865214d1344b10e5df4f
Vimal Patel <vimal.patel.serpentcs@gmail.com>
2021-06-23 15:49:53 +0530

    [MIG]account_bill_line_distribution: migrated to v14

--- account_bill_line_distribution/models/account_move_line.py
+++ account_bill_line_distribution/models/account_move_line.py
@@ -1,57 +1,53 @@
-# Copyright (C) 2019 Open Source Integrators
+# Copyright (C) 2021 Open Source Integrators
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-from odoo import api, fields, models, _
+from odoo import api, fields, models, _, SUPERUSER_ID
+from odoo.tests.common import Form
+
 from odoo.exceptions import ValidationError
 
 
-class AccountInvoiceLine(models.Model):
-    _inherit = 'account.invoice.line'
+class AccountMoveLine(models.Model):
+    _inherit = 'account.move.line'
 
     distribution_ids = fields.One2many(
         'account.invoice.line.distribution', 'invoice_line_id',
         string='Distribution', copy=True)
 
     @api.onchange('quantity', 'price_unit')
-    def onchange_comp_amount_percent(self):
-        for dist_id in self.distribution_ids:
-            dist_id.amount = 0.00
-            dist_id.percent = 0.00
-            dist_id.onchange_amount_total()
-            dist_id.onchange_percent_total()
+    def _onchange_comp_amount_percent(self):
+        for dist_line in self.distribution_ids:
+            dist_line.update({
+                              'amount': 0.00,
+                              'percent':0.00
+                              })
+            dist_line._onchange_amount_total()
+            dist_line._onchange_percent_total()
 
     def get_default_distribution(self):
         return {'percent': 100.00,
                 'company_id':
                     self.env.context.get('company_id') or
                     self.company_id.id or
-                    self.invoice_id.company_id.id or
+                    self.move_id.company_id.id or
                     self.env.user.company_id.id}
 
-    @api.model
-    def create(self, vals):
-        if 'distribution_ids' not in vals or not vals['distribution_ids']:
-            if vals.get('company_id', False):
-                vals.update({
-                    'distribution_ids': [(0, 0, self.
-                            with_context({'company_id': vals['company_id']}).
-                            get_default_distribution())]})
-            else:
-                vals.update({
-                            'distribution_ids':
-                            [(0, 0, self.get_default_distribution())]})
-        return super().create(vals)
+    @api.model_create_multi
+    def create(self, vals_list):
+        lines = super(AccountMoveLine, self).create(vals_list)
+        invoice_line = lines.filtered(lambda line: line.move_id.move_type in ('out_invoice', 'in_invoice', 'out_refund', 'in_refund', 'out_receipt', 'in_receipt'))
+        if not invoice_line.distribution_ids:
+            invoice_line.write({'distribution_ids' : [(0, 0, invoice_line.get_default_distribution())]})
+        return lines
 
     @api.constrains('distribution_ids')
     def _check_percentage(self):
-        for line in self:
-            if line.distribution_ids:
-                total = 0.00
-                for dist_id in line.distribution_ids:
-                    total += dist_id.percent
-                if total != 100.00:
-                    raise ValidationError(_(
-                        "The distribution for {} does not total to 100%.").
-                        format(line.name))
+        for line in self.filtered(lambda line: line.move_id.move_type in ('out_invoice', 'in_invoice', 'out_refund', 'in_refund', 'out_receipt', 'in_receipt')
+                                  and not line.tax_exigible):
+            total = sum(line.distribution_ids.mapped('percent'))
+            if total != 100.00:
+                raise ValidationError(_(
+                    "The distribution for {} does not total to 100%.").
+                    format(line.name))
 
     @api.onchange('distribution_ids')
     def _onchange_distribution_ids_percent(self):
@@ -67,7 +63,7 @@ class AccountInvoiceLine(models.Model):
             if 'index' in locals():
                 self.distribution_ids[index].percent = 100.00 - percentage
         else:
-            self.distribution_ids = [self.get_default_distribution()]
+            self.distribution_ids = [(0, 0, self.get_default_distribution())]
 
     @api.onchange('distribution_ids')
     def _onchange_distribution_ids_amount(self):
@@ -84,4 +80,4 @@ class AccountInvoiceLine(models.Model):
                 self.distribution_ids[index].amount = \
                     self.price_subtotal - amount
         else:
-            self.distribution_ids = [self.get_default_distribution()]
+            self.distribution_ids = [(0, 0, self.get_default_distribution())]
