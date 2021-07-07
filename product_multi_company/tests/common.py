ca7b8379d51012b1039f814281b2327351b567de
Denis Roussel <denis.roussel@acsone.eu>
2021-07-07 08:55:42 +0200

    [40.0][IMP] product_multi_company: Add methods helpers for tests

--- product_multi_company/tests/common.py
+++ product_multi_company/tests/common.py
@@ -5,14 +5,8 @@
 
 class ProductMultiCompanyCommon(object):
     @classmethod
-    def setUpClass(cls):
-        super().setUpClass()
-        cls.env = cls.env(context=dict(cls.env.context, tracking_disable=True))
-        groups = cls.env.ref("base.group_system")
+    def _create_products(cls):
         cls.product_obj = cls.env["product.product"]
-        cls.company_obj = cls.env["res.company"]
-        cls.company_1 = cls.company_obj.create({"name": "Test company 1"})
-        cls.company_2 = cls.company_obj.create({"name": "Test company 2"})
         cls.product_company_none = cls.product_obj.create(
             {
                 "name": "Product without company",
@@ -38,11 +32,14 @@ class ProductMultiCompanyCommon(object):
                 "company_ids": [(6, 0, (cls.company_1 + cls.company_2).ids)],
             }
         )
+
+    @classmethod
+    def _create_users(cls):
         cls.user_company_1 = cls.env["res.users"].create(
             {
                 "name": "User company 1",
                 "login": "user_company_1",
-                "groups_id": [(6, 0, groups.ids)],
+                "groups_id": [(6, 0, cls.groups.ids)],
                 "company_id": cls.company_1.id,
                 "company_ids": [(6, 0, cls.company_1.ids)],
             }
@@ -51,8 +48,19 @@ class ProductMultiCompanyCommon(object):
             {
                 "name": "User company 2",
                 "login": "user_company_2",
-                "groups_id": [(6, 0, groups.ids)],
+                "groups_id": [(6, 0, cls.groups.ids)],
                 "company_id": cls.company_2.id,
                 "company_ids": [(6, 0, cls.company_2.ids)],
             }
         )
+
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.env = cls.env(context=dict(cls.env.context, tracking_disable=True))
+        cls.groups = cls.env.ref("base.group_system")
+        cls.company_obj = cls.env["res.company"]
+        cls.company_1 = cls.company_obj.create({"name": "Test company 1"})
+        cls.company_2 = cls.company_obj.create({"name": "Test company 2"})
+        cls._create_products()
+        cls._create_users()
