6b204ea985dacbbd9f221a68d1a3591cfe650787
Anna Janiszewska <anna.janiszewska@camptocamp.com>
2021-08-25 16:46:22 +0200

    [14.0] add stock_intercompany module

---
+++ stock_intercompany/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2021 Camptocamp
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Stock Intercompany Delivery-Reception",
+    "Summary": "Module that adds possibility for intercompany Delivery-Reception",
+    "version": "14.0.1.1.1",
+    "author": "Camptocamp, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/multi-company",
+    "category": "Warehouse Management",
+    "depends": ["stock"],
+    "installable": True,
+    "license": "AGPL-3",
+    "data": [
+        "views/res_config_settings.xml",
+    ],
+}
