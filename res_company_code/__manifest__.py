2daf116fe479e908997dd92290c314f5eeffac3d
Kevin Khao <kevinkhao@gmail.com>
2021-08-30 10:27:45 +0300

    [MIG] res_company_code: Migration to 14.0

--- res_company_code/__manifest__.py
+++ res_company_code/__manifest__.py
@@ -5,7 +5,7 @@
 {
     "name": "Company Code",
     "summary": "Add 'code' field on company model",
-    "version": "13.0.1.0.0",
+    "version": "14.0.1.0.0",
     "category": "Tools",
     "author": "GRAP, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/multi-company",
