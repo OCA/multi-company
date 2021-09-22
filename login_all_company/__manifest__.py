971ff9963b1a59b740107038335f4f4aea5ac38f
Enric Tobella <etobella@creublanca.es>
2021-09-22 11:24:50 +0200

    [ADD] login_all_company

---
+++ login_all_company/__manifest__.py
@@ -0,0 +1,14 @@
+# Copyright 2021 Creu Blanca
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Login All Company",
+    "summary": """
+        Access all your companies when you log in""",
+    "version": "14.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "Creu Blanca,Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/multi-company",
+    "depends": ["web"],
+    "data": ["templates/assets.xml"],
+}
