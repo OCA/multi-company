eba836dca0db29c588ffaa0ea87002a4dbe9c319
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2021-10-22 16:27:16 +0200

    [FIX] pre-commit [ci skip]

--- purchase_sale_inter_company/__manifest__.py
+++ purchase_sale_inter_company/__manifest__.py
@@ -8,7 +8,7 @@
     "summary": "Intercompany PO/SO rules",
     "version": "13.0.1.0.5",
     "category": "Purchase Management",
-    "website": "http://www.github.com/OCA/multi-company",
+    "website": "https://github.com/OCA/multi-company",
     "author": "Odoo SA, Akretion, Tecnativa, Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "installable": True,
