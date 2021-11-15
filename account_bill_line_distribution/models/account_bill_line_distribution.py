b077ae571c67c0e5194f6d87f5573d5a15cf2926
Vimal Patel <vimal.patel.serpentcs@gmail.com>
2021-11-15 17:56:21 +0530

    [IMP/FIX]account_bill_line_distribution: fixed the traceback error and improved the code

--- account_bill_line_distribution/models/account_bill_line_distribution.py
+++ account_bill_line_distribution/models/account_bill_line_distribution.py
@@ -1,7 +1,8 @@
 # Copyright (C) 2021 Open Source Integrators
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-from odoo import api, fields, models,_
+from odoo import _, api, fields, models
 from odoo.exceptions import ValidationError
+from odoo.tools import float_is_zero
 
 
 class AccountInvoiceLineDistribution(models.Model):
@@ -19,7 +20,7 @@ class AccountInvoiceLineDistribution(models.Model):
             or self.env.user.company_id.id
         )
 
-    percent = fields.Float(string="Percentage")
+    percent = fields.Float(string="Percentage", default="100")
     amount = fields.Float(string="Amount")
     invoice_line_id = fields.Many2one(
         "account.move.line", string="Bill Line", ondelete="cascade", check_company=True
@@ -33,15 +34,31 @@ class AccountInvoiceLineDistribution(models.Model):
     #        [('line_company_uniq', 'UNIQUE (invoice_line_id, company_id)',
     #          'You cannot have the same company twice in a distribution!')]
 
-    @api.constrains("invoice_line_id","company_id")
+    @api.constrains("invoice_line_id", "company_id")
     def _check_unique_company_distribution(self):
         for rec in self:
-            distribution_lines = rec.search_count([('invoice_line_id','=',rec.invoice_line_id.id),
-                                                    ('company_id','=',rec.company_id.id),
-                                                    ('id','!=',rec.id)])
+            distribution_lines = rec.search_count(
+                [
+                    ("invoice_line_id", "=", rec.invoice_line_id.id),
+                    ("company_id", "=", rec.company_id.id),
+                    ("id", "!=", rec.id),
+                ]
+            )
             if distribution_lines:
-                raise ValidationError(_('You cannot have the same company twice in a distribution!'))
-            
+                raise ValidationError(
+                    _("You cannot have the same company twice in a distribution!")
+                )
+
+    @api.constrains("percent", "amount")
+    def _check_negative_amount(self):
+        for rec in self:
+            if rec.percent < 0.0:
+                raise ValidationError(_("Amount must be positive value!"))
+            elif float_is_zero(
+                rec.percent, precision_digits=rec.company_id.currency_id.decimal_places
+            ):
+                raise ValidationError(_("Amount must be bigger then 0.0 (zero)."))
+
     @api.onchange("percent")
     def _onchange_percent_total(self):
         for dist in self:
