63fd0da45c6df130f9d55a84597700f76e7f54f6
Etienne Ferriere <eferriere@xcglpt039.local>
2021-12-01 16:30:20 +0100

    [IMP] 12_mig_ir_actions_report_multi_company: black, isort, prettier

--- ir_actions_report_multi_company/__manifest__.py
+++ ir_actions_report_multi_company/__manifest__.py
@@ -2,18 +2,12 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    'name': 'Ir Actions Report Multi Company',
-    'summary': 'Make Report Actions multi-company aware',
-    'version': '12.0.1.0.0',
-    'license': 'AGPL-3',
-    'author': 'ACSONE SA/NV,'
-              'Odoo Community Association (OCA)',
-    'website': 'https://github.com/OCA/multi-company',
-    'depends': [
-        'base',
-    ],
-    'data': [
-        'views/ir_actions_report_view.xml',
-        'security/multi_company.xml',
-    ],
+    "name": "Ir Actions Report Multi Company",
+    "summary": "Make Report Actions multi-company aware",
+    "version": "12.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "ACSONE SA/NV," "Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/multi-company",
+    "depends": ["base"],
+    "data": ["views/ir_actions_report_view.xml", "security/multi_company.xml"],
 }
