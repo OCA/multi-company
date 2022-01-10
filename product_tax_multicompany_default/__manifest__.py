2376edfb3d500e0f607e82a38f01c41827de7588
aromera <aromera@pmsastro.es>
2022-01-10 13:58:35 +0100

    [MIG] product_tax_multicompany_default: Migration to 14.0

--- product_tax_multicompany_default/__manifest__.py
+++ product_tax_multicompany_default/__manifest__.py
@@ -4,7 +4,7 @@
 
 {
     "name": "Product Tax Multi Company Default",
-    "version": "13.0.1.0.0",
+    "version": "14.0.1.0.0",
     "category": "Account",
     "website": "https://github.com/OCA/multi-company",
     "author": "Tecnativa, Odoo Community Association (OCA)",
