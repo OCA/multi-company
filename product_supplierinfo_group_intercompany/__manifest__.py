4f852178be79c96782634b6faf5f81b9e7148eb2
Kevin Khao <kevinkhao@gmail.com>
2022-02-16 14:07:50 +0300

    [ADD] product_supplierinfo_group_intercompany

---
+++ product_supplierinfo_group_intercompany/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2022 Akretion
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Product Supplierinfo Group Intercompany Sequence",
+    "summary": """
+        Add sequence field on grouped pricelist items""",
+    "version": "14.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "Akretion,Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/multi-company",
+    "depends": ["product_supplier_intercompany", "product_supplierinfo_group"],
+    "data": [
+        "security/ir_rule.xml",
+        "views/product_supplierinfo_group.xml",
+        "views/product_pricelist.xml",
+    ],
+    "demo": [],
+}
---
+++ product_supplierinfo_group_intercompany/readme/DESCRIPTION.rst
@@ -0,0 +1,3 @@
+Glue module for product_supplierinfo_group and purchase_supplier_intercompany.
+Allows easy reordering of supplierinfo groups (supplier price lists) and restricts
+edition of these groups if they were automatically generated from an intercompany pricelist.
