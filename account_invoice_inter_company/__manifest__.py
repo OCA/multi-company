302367e194d2ddfcc80be8f53537224a1cf266d4
Christopher Ormaza <chris.ormaza@forgeflow.com>
2022-02-18 10:49:52 -0500

    [15.0][MIG] account_invoice_inter_company

--- account_invoice_inter_company/__manifest__.py
+++ account_invoice_inter_company/__manifest__.py
@@ -7,12 +7,12 @@
 {
     "name": "Inter Company Invoices",
     "summary": "Intercompany invoice rules",
-    "version": "14.0.1.0.3",
+    "version": "15.0.1.0.0",
     "category": "Accounting & Finance",
     "website": "https://github.com/OCA/multi-company",
     "author": "Odoo SA, Akretion, Odoo Community Association (OCA)",
     "license": "AGPL-3",
-    "depends": ["account"],
+    "depends": ["account", "base_setup"],
     "data": ["views/account_move_views.xml", "views/res_config_settings_view.xml"],
     "installable": True,
 }
