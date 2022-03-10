f8b83926b6ce73808368636cd369b6d53d5846a4
Jasmin Solanki <jasmin.solanki@forgeflow.com>
2022-03-10 15:00:18 +0530

    [MIG] mail_template_multi_company: Migration to 15.0

--- mail_template_multi_company/__manifest__.py
+++ mail_template_multi_company/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Mail Template Multi Company",
-    "version": "14.0.1.0.0",
+    "version": "15.0.1.0.0",
     "license": "AGPL-3",
     "author": "ACSONE SA/NV," "Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/multi-company",
