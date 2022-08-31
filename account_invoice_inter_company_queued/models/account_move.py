d430ed928bcb2b223fe72a3831d87ab25c287db2
Ilyas <irazor147@gmail.com>
2022-08-31 17:35:29 +0500

    [14.0][FIX] account_invoice_inter_company_queued: check record exists before creating counterpart invoice

--- account_invoice_inter_company_queued/models/account_move.py
+++ account_invoice_inter_company_queued/models/account_move.py
@@ -8,7 +8,8 @@ class AccountMove(models.Model):
     def _post(self, soft=True):
         self = self.with_context(account_invoice_inter_company_queued=True)
         res = super(AccountMove, self)._post(soft=soft)
-        dest_company = self._find_company_from_invoice_partner()
-        if dest_company and not self.auto_generated:
-            self.sudo().with_delay().create_counterpart_invoices()
+        for src_invoice in self.filtered(lambda x: x.is_invoice()):
+            dest_company = src_invoice._find_company_from_invoice_partner()
+            if dest_company and not src_invoice.auto_generated:
+                src_invoice.sudo().with_delay().create_counterpart_invoices()
         return res
