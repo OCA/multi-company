69416575cc0613620e8c39841ea00ebe5f82b035
Sylvain LE GAL <sylvain.legal@grap.coop>
2022-10-14 14:07:05 +0200

    [IMP] pos_restaurant_multi_company: black, isort, prettier

--- pos_restaurant_multi_company/hooks.py
+++ pos_restaurant_multi_company/hooks.py
@@ -2,7 +2,7 @@
 # @author: Sylvain LE GAL (https://twitter.com/legalsylvain)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import api, SUPERUSER_ID
+from odoo import SUPERUSER_ID, api
 
 
 def post_init_hook(cr, registry):
@@ -10,14 +10,12 @@ def post_init_hook(cr, registry):
         env = api.Environment(cr, SUPERUSER_ID, {})
         # Try to guess company id, based on the optional field pos_config_id
         # of the model restaurant.floor
-        floors = env["restaurant.floor"].search(
-            [('pos_config_id', "!=", False)])
+        floors = env["restaurant.floor"].search([("pos_config_id", "!=", False)])
         for floor in floors:
             floor.company_id = floor.pos_config_id.company_id
 
         # Otherwise, set company_id to False
-        floors = env["restaurant.floor"].search(
-            [('pos_config_id', "=", False)])
+        floors = env["restaurant.floor"].search([("pos_config_id", "=", False)])
         floors.write({"company_id": False})
 
         # Initialize printers with no company
