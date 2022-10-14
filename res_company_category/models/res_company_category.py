bc36e80a7e2634294c54dd745d0f9f62fefb758d
Sylvain LE GAL <sylvain.legal@grap.coop>
2022-10-14 12:26:35 +0200

    [IMP] res_company_category: black, isort, prettier

--- res_company_category/models/res_company_category.py
+++ res_company_category/models/res_company_category.py
@@ -6,56 +6,62 @@ from odoo import api, fields, models
 
 
 class ResCompanyCategory(models.Model):
-    _name = 'res.company.category'
-    _description = 'Company Categories'
-    _order = 'complete_name'
+    _name = "res.company.category"
+    _description = "Company Categories"
+    _order = "complete_name"
 
     _TYPE_SELECTION = [
-        ('normal', 'Normal'),
-        ('view', 'View'),
+        ("normal", "Normal"),
+        ("view", "View"),
     ]
 
     # Fields Section
-    name = fields.Char(string='Name', required=True)
+    name = fields.Char(string="Name", required=True)
 
     type = fields.Selection(
-        string='Type', selection=_TYPE_SELECTION,
-        required=True, default='normal')
+        string="Type", selection=_TYPE_SELECTION, required=True, default="normal"
+    )
 
     parent_id = fields.Many2one(
-        string='Parent Category', comodel_name='res.company.category',
-        domain=[('type', '=', 'view')])
+        string="Parent Category",
+        comodel_name="res.company.category",
+        domain=[("type", "=", "view")],
+    )
 
     child_ids = fields.One2many(
-        string='Category Childs', comodel_name='res.company.category',
-        inverse_name='parent_id')
+        string="Category Childs",
+        comodel_name="res.company.category",
+        inverse_name="parent_id",
+    )
 
     company_ids = fields.One2many(
-        string='Companies', comodel_name='res.company',
-        inverse_name='category_id')
+        string="Companies", comodel_name="res.company", inverse_name="category_id"
+    )
 
     company_qty = fields.Integer(
-        string='Companies Quantity', compute='_compute_company_qty',
-        store=True)
+        string="Companies Quantity", compute="_compute_company_qty", store=True
+    )
 
     complete_name = fields.Char(
-        string='Complete Name', compute='_compute_complete_name', store=True)
+        string="Complete Name", compute="_compute_complete_name", store=True
+    )
 
     # Compute Section
-    @api.depends('parent_id.complete_name', 'name')
+    @api.depends("parent_id.complete_name", "name")
     def _compute_complete_name(self):
         for category in self:
             if category.parent_id:
-                category.complete_name = '%s / %s' % (
-                    category.parent_id.complete_name, category.name)
+                category.complete_name = "%s / %s" % (
+                    category.parent_id.complete_name,
+                    category.name,
+                )
             else:
                 category.complete_name = category.name
 
-    @api.depends('company_ids.category_id', 'child_ids.company_qty')
+    @api.depends("company_ids.category_id", "child_ids.company_qty")
     def _compute_company_qty(self):
         for category in self:
-            if category.type == 'normal':
+            if category.type == "normal":
                 category.company_qty = len(category.company_ids)
             else:
-                category.company_qty = sum(
-                    category.mapped('child_ids.company_qty'))
+                category.company_qty = sum(category.mapped("child_ids.company_qty"))
