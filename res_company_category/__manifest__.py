738c1ff2395f37ea89c452776652f617bee3aae5
Sylvain LE GAL <sylvain.legal@grap.coop>
2022-10-14 12:26:35 +0200

    [IMP] res_company_category: black, isort, prettier

--- res_company_category/__manifest__.py
+++ res_company_category/__manifest__.py
@@ -3,29 +3,29 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
-    'name': 'Company Categories',
-    'version': '12.0.1.0.0',
-    'category': 'Tools',
-    'author': 'GRAP, Odoo Community Association (OCA)',
-    'website': 'https://github.com/OCA/multi-company',
-    'license': 'AGPL-3',
-    'depends': [
-        'base',
+    "name": "Company Categories",
+    "version": "12.0.1.0.0",
+    "category": "Tools",
+    "author": "GRAP, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/multi-company",
+    "license": "AGPL-3",
+    "depends": [
+        "base",
     ],
-    'data': [
-        'security/ir.model.access.csv',
-        'views/view_res_company.xml',
-        'views/view_res_company_category.xml',
+    "data": [
+        "security/ir.model.access.csv",
+        "views/view_res_company.xml",
+        "views/view_res_company_category.xml",
     ],
-    'demo': [
-        'demo/res_groups.xml',
-        'demo/res_company_category.xml',
-        'demo/res_company.xml',
+    "demo": [
+        "demo/res_groups.xml",
+        "demo/res_company_category.xml",
+        "demo/res_company.xml",
     ],
-    'installable': True,
-    'images': [
-        'static/description/res_company_category_form.png',
-        'static/description/res_company_category_tree.png',
-        'static/description/res_company_form.png',
+    "installable": True,
+    "images": [
+        "static/description/res_company_category_form.png",
+        "static/description/res_company_category_tree.png",
+        "static/description/res_company_form.png",
     ],
 }
