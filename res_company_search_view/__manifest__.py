ef41ba95aeff62518bc1ffc070e111f5ff0c4f3f
Sylvain LE GAL <sylvain.legal@grap.coop>
2022-10-14 12:15:21 +0200

    [ADD] res_company_search_view

---
+++ res_company_search_view/__manifest__.py
@@ -0,0 +1,21 @@
+# Copyright (C) 2022 - Today: GRAP (http://www.grap.coop)
+# @author: Sylvain LE GAL (https://twitter.com/legalsylvain)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Company - Search View",
+    "summary": "Add a search view for company model",
+    "version": "16.0.1.0.0",
+    "category": "Tools",
+    "author": "GRAP, Odoo Community Association (OCA)",
+    "maintainers": ["legalsylvain"],
+    "website": "https://github.com/OCA/multi-company",
+    "license": "AGPL-3",
+    "depends": [
+        "base",
+    ],
+    "data": [
+        "views/view_res_company.xml",
+    ],
+    "installable": True,
+}
