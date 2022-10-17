7b05202ac4af9c9ade634b9ae75a7ec592923701
Sylvain LE GAL <sylvain.legal@grap.coop>
2022-10-17 13:08:53 +0200

    [PORT] pos_restaurant_multi_company (from 12.0 to 16.0)

--- pos_restaurant_multi_company/models/restaurant_floor.py
+++ pos_restaurant_multi_company/models/restaurant_floor.py
@@ -2,27 +2,16 @@
 # @author: Sylvain LE GAL (https://twitter.com/legalsylvain)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import _, api, fields, models
-from odoo.exceptions import UserError
+from odoo import fields, models
 
 
 class RestaurantFloor(models.Model):
     _inherit = "restaurant.floor"
 
     company_id = fields.Many2one(
-        "res.company",
+        comodel_name="res.company",
         string="Company",
+        related="pos_config_id.company_id",
         index=True,
-        default=lambda self: self.env.user.company_id,
+        store=True,
     )
-
-    @api.onchange("pos_config_id")
-    def _onchange_pos_config_id(self):
-        if self.pos_config_id:
-            self.company_id = self.pos_config_id.company_id
-
-    @api.constrains("company_id", "pos_config_id")
-    def _check_company_pos_config(self):
-        for floor in self.filtered(lambda x: x.company_id and x.pos_config_id):
-            if floor.company_id != floor.pos_config_id.company_id:
-                raise UserError(_("Incorrect values for company and pos config"))
