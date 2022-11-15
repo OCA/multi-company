6bc0e410e18cc243df4a46b4ef324bf021cb5b7a
Víctor Martínez <victor.martinez@tecnativa.com>
2022-11-15 10:22:53 +0100

    [MIG] account_multicompany_easy_creation: Migation to 15.0
    TT40354

--- account_multicompany_easy_creation/tests/test_easy_creation.py
+++ account_multicompany_easy_creation/tests/test_easy_creation.py
@@ -1,14 +1,24 @@
-# Copyright 2021 Tecnativa - Víctor Martínez
+# Copyright 2021-2022 Tecnativa - Víctor Martínez
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl)
 
-from odoo.tests import Form, common
+from odoo.tests import Form, common, new_test_user
+from odoo.tests.common import users
 
 
-class TestEasyCreation(common.SavepointCase):
+class TestEasyCreation(common.TransactionCase):
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
-        cls.user = cls.env.ref("base.user_admin")
+        cls.env.ref("base.user_admin").write(
+            {
+                "groups_id": [(4, cls.env.ref("account.group_account_user").id)],
+            }
+        )
+        new_test_user(
+            cls.env,
+            login="test-user",
+            groups="account.group_account_user,base.group_system,base.group_partner_manager",
+        )
         cls.chart_template_id = cls.env["account.chart.template"].search([], limit=1)
         cls.sale_tax_template = cls.env["account.tax.template"].search(
             [("type_tax_use", "=", "sale")], limit=1
@@ -24,7 +34,9 @@ class TestEasyCreation(common.SavepointCase):
 
     def test_wizard_easy_creation(self):
         wizard_form = Form(
-            self.env["account.multicompany.easy.creation.wiz"].with_user(self.user)
+            self.env["account.multicompany.easy.creation.wiz"].with_context(
+                allowed_company_ids=self.env.company.ids
+            )
         )
         wizard_form.name = "test_company"
         wizard_form.default_purchase_tax_id = self.purchase_tax_template
@@ -34,6 +46,8 @@ class TestEasyCreation(common.SavepointCase):
         wizard_form.force_sale_tax = True
         wizard_form.force_purchase_tax = True
         wizard_form.default_sale_tax_id = self.sale_tax_template
+        for user in self.env["res.users"].search([]):
+            wizard_form.user_ids.add(user)
         record = wizard_form.save()
         record.action_accept()
         self.assertEqual(record.new_company_id.name, "test_company")
@@ -44,3 +58,7 @@ class TestEasyCreation(common.SavepointCase):
         self._test_model_items("account.tax", record.new_company_id)
         self._test_model_items("account.account", record.new_company_id)
         self._test_model_items("account.journal", record.new_company_id)
+
+    @users("test-user")
+    def test_wizard_easy_creation_test_user(self):
+        self.test_wizard_easy_creation()
