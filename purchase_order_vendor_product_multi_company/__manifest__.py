bf72418323c2d8dba80b434643c5cf094343cc37
Ilyas <irazor147@gmail.com>
2022-11-18 18:15:34 +0500

    [ADD] purchase_order_vendor_product_multi_company

---
+++ purchase_order_vendor_product_multi_company/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    "name": "Purchase Order Vendor Product Multi Company",
+    "version": "14.0.1.0.0",
+    "summary": " ",
+    "author": "Ilyas, Ooops, Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "category": "Purchase",
+    "website": "https://github.com/OCA/multi-company",
+    "depends": ["product_multi_company", "purchase_order_vendor_product"],
+    "installable": True,
+    "application": False,
+    "auto_install": True,
+}
---
+++ purchase_order_vendor_product_multi_company/models/product_product.py
@@ -0,0 +1,31 @@
+from odoo import exceptions, models
+
+
+class ProductProduct(models.Model):
+    _inherit = "product.product"
+
+    def _name_search(
+        self, name, args=None, operator="ilike", limit=100, name_get_uid=None
+    ):
+        for index, domain_triplet in enumerate(args):
+            if domain_triplet[0] == "variant_seller_ids.name" and domain_triplet[2]:
+                product_ids = []
+                sup_infos = self.env["product.supplierinfo"].search(
+                    [
+                        ("name", "=", domain_triplet[2]),
+                    ]
+                )
+                for sup_info in sup_infos:
+                    if sup_info.product_id:
+                        product_ids.append(sup_info.product_id.id)
+                    else:
+                        try:
+                            product_ids.extend(
+                                sup_info.product_tmpl_id.product_variant_ids.ids
+                            )
+                        except exceptions.AccessError:
+                            pass
+                args[index] = ["id", "in", product_ids]
+        return super(ProductProduct, self)._name_search(
+            name, args=args, operator=operator, limit=limit, name_get_uid=name_get_uid
+        )
