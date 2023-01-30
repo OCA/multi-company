188adf25edfb37d72715cd88b311dc17ac6d5a11
Eduardo De Miguel <edu@moduon.team>
2023-01-30 10:21:40 +0100

    [FIX] product_tax_multicompany_default: Overwrite taxes instead of adding it

--- product_tax_multicompany_default/models/product.py
+++ product_tax_multicompany_default/models/product.py
@@ -1,6 +1,9 @@
 # Copyright 2017 Carlos Dauden - Tecnativa <carlos.dauden@tecnativa.com>
 # Copyright 2018 Vicent Cubells - Tecnativa <vicent.cubells@tecnativa.com>
+# Copyright 2023 Eduardo de Miguel - Moduon <edu@moduon.team>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from typing import List
 
 from odoo import api, models
 
@@ -24,6 +27,32 @@ class ProductTemplate(models.Model):
             )
         return taxes_ids
 
+    def _delete_product_taxes(
+        self,
+        excl_customer_tax_ids: List[int] = None,
+        excl_supplier_tax_ids: List[int] = None,
+    ):
+        """Delete taxes from product excluding chosen taxes
+
+        :param excl_customer_tax_ids: Excluded customer tax ids
+        :param excl_supplier_tax_ids: Excluded supplier tax ids
+        """
+        tax_where = " AND tax_id NOT IN %s"
+        # Delete customer taxes
+        customer_sql = "DELETE FROM product_taxes_rel WHERE prod_id IN %s"
+        customer_sql_params = [tuple(self.ids)]
+        if excl_customer_tax_ids:
+            customer_sql += tax_where
+            customer_sql_params.append(tuple(excl_customer_tax_ids))
+        self.env.cr.execute(customer_sql + ";", customer_sql_params)
+        # Delete supplier taxes
+        supplier_sql = "DELETE FROM product_supplier_taxes_rel WHERE prod_id IN %s"
+        supplier_sql_params = [tuple(self.ids)]
+        if excl_supplier_tax_ids:
+            supplier_sql += tax_where
+            supplier_sql_params.append(tuple(excl_supplier_tax_ids))
+        self.env.cr.execute(supplier_sql + ";", supplier_sql_params)
+
     def set_multicompany_taxes(self):
         self.ensure_one()
         user_company = self.env.company
@@ -41,6 +70,11 @@ class ProductTemplate(models.Model):
         )
         default_supplier_tax_ids = obj.taxes_by_company(
             "account_purchase_tax_id", user_company
+        )
+        # Clean taxes from other companies (cannot replace it with sudo)
+        self._delete_product_taxes(
+            excl_customer_tax_ids=customer_tax_ids,
+            excl_supplier_tax_ids=supplier_tax_ids,
         )
         # Use list() to copy list
         match_customer_tax_ids = (
