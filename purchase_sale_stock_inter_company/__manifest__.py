094b2c625c0bb07f195cd998c01d086899f2578d
chafique.delli <chafique.delli@akretion.com>
2023-02-13 10:11:40 +0100

    [IMP] purchase_sale_stock_inter_company: pre-commit stuff

--- purchase_sale_stock_inter_company/__manifest__.py
+++ purchase_sale_stock_inter_company/__manifest__.py
@@ -6,7 +6,7 @@
 {
     "name": "Inter Company Module for Purchase to Sale Order with warehouse",
     "summary": "Intercompany PO/SO rules with warehouse",
-    "version": "15.0.1.0.0",
+    "version": "16.0.1.0.0",
     "category": "Purchase Management",
     "website": "https://github.com/OCA/multi-company",
     "author": "Odoo SA, Akretion, Tecnativa, Odoo Community Association (OCA)",
