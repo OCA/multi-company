95ef5a7e1ed3faa5b62bb33e2243219d082965c8
Mateu Griful <mateu.griful@forgeflow.com>
2023-03-15 12:30:41 +0100

    [12.0][IMP] purchas_sale_inter_company: sync lines and moves

--- purchase_sale_inter_company/models/stock_picking.py
+++ purchase_sale_inter_company/models/stock_picking.py
@@ -76,14 +76,17 @@ class StockPicking(models.Model):
             raise UserError(_("PO does not exist or has no receipts"))
         if self.intercompany_picking_id:
             dest_picking = self.intercompany_picking_id.sudo(intercompany_user.id)
-            for picking_line in self.move_ids_without_package:
-                picking_line.write({
-                    'qty_done': picking_line.reserved_availability,
+            for picking_move in self.move_ids_without_package:
+                dest_picking_move = dest_picking.sudo().move_ids_without_package.filtered(
+                    lambda l: l.product_id.id == picking_move.product_id.id)
+                dest_picking_move.sudo().write({
+                    'quantity_done': picking_move.quantity_done,
                 })
+            for picking_line in self.move_line_ids_without_package:
                 dest_picking_line = dest_picking.sudo().move_line_ids_without_package.filtered(
                     lambda l: l.product_id.id == picking_line.product_id.id)
                 dest_picking_line.sudo().write({
-                    'qty_done': picking_line.reserved_availability,
+                    'qty_done': picking_line.qty_done,
                 })
 
     def action_generate_backorder_wizard(self):
