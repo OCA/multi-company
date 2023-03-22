04686c011002ae9197e81dbcfc60387825b329d2
Hans Henrik Gabelgaard <hhg@gabelgaard.org>
2023-03-22 16:33:36 +0100

    [MIG] ir_filters_multi_company: Migration to 15.0

--- ir_filters_multi_company/__manifest__.py
+++ ir_filters_multi_company/__manifest__.py
@@ -2,15 +2,14 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    'name': 'User-defined Filters Multi Company',
-    'summary': """
+    "name": "User-defined Filters Multi Company",
+    "summary": """
         This module add multi-company management to user-defined filters""",
-    'version': '12.0.1.0.0',
-    'license': 'AGPL-3',
-    'author': 'ACSONE SA/NV,'
-              'Odoo Community Association (OCA)',
-    'website': 'https://github.com/OCA/multi-company',
-    'depends': ['base'],
-    'data': ['security/ir_filters.xml', 'views/ir_filters.xml'],
+    "version": "15.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "ACSONE SA/NV," "Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/multi-company",
+    "depends": ["base"],
+    "data": ["security/ir_filters.xml", "views/ir_filters.xml"],
     "post_init_hook": "post_init_hook",
 }
