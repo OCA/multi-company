03bc61bd158be2fc1f936291bff63e9e9e9a1bb9
Miquel Raïch <miquel.raich@forgeflow.com>
2023-04-11 17:18:54 +0200

    [FIX] product_intercompany_account: pass force_company in context

--- product_intercompany_account/models/sale_order_line.py
+++ product_intercompany_account/models/sale_order_line.py
@@ -1,17 +1,19 @@
-from odoo import models
+from odoo import api, models
 
 
 class SaleOrderLine(models.Model):
     _inherit = "sale.order.line"
 
+    @api.multi
     def _prepare_invoice_line(self, qty):
         res = super(SaleOrderLine, self)._prepare_invoice_line(qty)
 
         our_companies = self.env['res.company'].search(
             [('partner_id', '=', self.order_id.partner_id.id)]
         )
-        product_level = self.product_id.property_account_income_intercompany
-        categ = self.product_id.categ_id.property_account_income_categ_intercompany
+        product = self.product_id.with_context(force_company=self.company_id.id)
+        product_level = product.property_account_income_intercompany
+        categ = product.categ_id.property_account_income_categ_intercompany
         if our_companies and (product_level or categ):
             account = product_level or categ
             res.update({'account_id': account.id})
