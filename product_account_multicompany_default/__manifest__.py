311a703bd52ae42714fe49624187a16383653c8d
Jairo Llopis <yajo.sk8@gmail.com>
2023-04-28 13:03:29 +0100

    [ADD] product_account_multicompany_default: propagate product account to other companies
    This module extends the functionality of products and accounting to allow you
    to propagate multi-company product accounts from the current company to all
    other companies where you have access rights.
    
    @moduon MT-2626

---
+++ product_account_multicompany_default/__manifest__.py
@@ -0,0 +1,20 @@
+# Copyright 2023 Moduon Team S.L.
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl-3.0)
+
+{
+    "name": "Product Account Multi-Company Default",
+    "summary": "Set a default account for all companies of a product",
+    "version": "16.0.0.1.0",
+    "development_status": "Alpha",
+    "category": "Accounting/Accounting",
+    "website": "https://github.com/OCA/multi-company",
+    "author": "Moduon, Odoo Community Association (OCA)",
+    "maintainers": ["yajo"],
+    "license": "LGPL-3",
+    "depends": [
+        "account",
+    ],
+    "data": [
+        "views/product_template_view.xml",
+    ],
+}
---
+++ product_account_multicompany_default/readme/USAGE.rst
@@ -0,0 +1,5 @@
+To use this module, you need to:
+
+#. Go to a product's form view. It can be the product template or the variant.
+#. Enter the "Accounting" tab.
+#. Click on "Propagate expense/income account to other companies".
