8850ffe9406a970fc48cf5f08f646e912dafadbb
Lois Rilo <lois.rilo@forgeflow.com>
2023-07-25 16:49:39 +0200

    [MIG] multicompany_configuration: Migration to 15.0

--- multicompany_configuration/__manifest__.py
+++ multicompany_configuration/__manifest__.py
@@ -5,9 +5,9 @@
     "name": "Multicompany Configuration",
     "summary": """
         Simplify the configuration on multicompany environments""",
-    "version": "14.0.1.0.0",
+    "version": "15.0.1.0.0",
     "license": "AGPL-3",
-    "author": "CreuBlanca,Odoo Community Association (OCA)",
+    "author": "CreuBlanca, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/multi-company",
     "depends": ["base_sparse_field", "product"],
     "data": [
@@ -15,7 +15,12 @@
         "views/product_template.xml",
         "views/product_product.xml",
         "views/res_partner.xml",
-        "templates/assets.xml",
     ],
+    "assets": {
+        "web.assets_backend": [
+            "multicompany_configuration/static/src/js/multicompany_data_field.js",
+            "multicompany_configuration/static/src/scss/multicompany_data_field.scss",
+        ],
+    },
     "demo": [],
 }
