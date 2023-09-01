853b0cb79b9e7b1e6c5e6f2d58fe2494e18e1252
Adam Heinz <amh@metricwise.net>
2023-09-01 10:34:10 -0400

    [MIG] calendar_event_multi_company: Migration to 14.0

--- calendar_event_multi_company/__manifest__.py
+++ calendar_event_multi_company/__manifest__.py
@@ -5,7 +5,7 @@
     "name": "Calendar Event Multi Company",
     "summary": """
         This module add multi-company management to calendar events""",
-    "version": "12.0.1.0.0",
+    "version": "14.0.1.0.0",
     "license": "AGPL-3",
     "author": "ACSONE SA/NV," "Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/multi-company",
