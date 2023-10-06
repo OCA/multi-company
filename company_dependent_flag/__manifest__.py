e2715a5332c411b84126cc171fbfb89071853e12
David Beal <david.beal@akretion.com>
2023-10-06 10:02:47 +0200

    [ADD] 16.0 company_dependent_flag module

---
+++ company_dependent_flag/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    "name": "Company Dependent Flag",
+    "author": "Akretion,Odoo Community Association (OCA)",
+    "summary": "Apply css style to company dependent fields",
+    "version": "16.0.1.0.0",
+    "license": "AGPL-3",
+    "maintainer": ["bealdav"],
+    "website": "https://github.com/OCA/multi-company",
+    "category": "web",
+    "depends": [
+        "base",
+    ],
+    "data": [],
+    "installable": True,
+}
