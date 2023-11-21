79bdb7e063fe54869659c75e8da6e3a37bdabf71
Telmo Santos <telmo.santos@camptocamp.com>
2023-11-21 11:29:45 +0100

    Add purchase_sale_container_deposit_inter_company

---
+++ purchase_sale_container_deposit_inter_company/models/purchase_order.py
@@ -0,0 +1,22 @@
+# Copyright 2023 Camptocamp (<https://www.camptocamp.com>).
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from odoo import models
+
+
+class PurchaseOrder(models.Model):
+    _inherit = "purchase.order"
+
+    def _inter_company_create_sale_order(self, dest_company):
+        """We skip container deposit computation on SO since we are only copying
+        the values from the PO"""
+        self = self.with_context(skip_update_container_deposit=True)
+        return super(PurchaseOrder, self)._inter_company_create_sale_order(dest_company)
+
+    def _prepare_sale_order_line_data(self, purchase_line, dest_company, sale_order):
+        order_line = super()._prepare_sale_order_line_data(
+            purchase_line, dest_company, sale_order
+        )
+        # Copy value from PO to SO
+        order_line["is_container_deposit"] = purchase_line.is_container_deposit
+        return order_line
---
+++ purchase_sale_container_deposit_inter_company/readme/DESCRIPTION.rst
@@ -0,0 +1,2 @@
+In `product_packaging_container_deposit`, product packaging container deposit lines added automatically by the system are flagged with (`is_container=True`).
+That information needs to be propagated from the PO to the SO to allow the correct computation of the product packaging container deposit.
