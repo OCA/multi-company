361d4571769cc015cb0e4c9e3a975ecdfd0a60d7
Denis Craciun <denis.craciun@innovyou.co>
2023-11-23 12:50:21 +0100

    [14.0] ir_config_parameter_multi_company implementation

---
+++ ir_config_parameter_multi_company/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    "name": "IR Config Parameter Multi Company",
+    "summary": "Add res company field in ir config parameter",
+    "version": "14.0.1.0.0",
+    "category": "Tools",
+    "author": "Innovyou, Odoo Community Association (OCA)",
+    "maintainers": ["deniscraciungabriel", "eLBati"],
+    "website": "https://github.com/OCA/multi-company",
+    "license": "AGPL-3",
+    "data": ["views/ir_config_parameter_view.xml", "security/ir.model.access.csv"],
+    "images": [],
+    "installable": True,
+}
