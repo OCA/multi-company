ef360ed365332f3a23000660e759b2ea6b4c2548
Alessandro Uffreduzzi <alessandro.uffreduzzi@pytech.it>
2023-12-06 15:22:17 +0100

    [FIX]purchase_sale_inter_company: handle pickings with lots/serials
    Previously an intercompany picking with tracked products would simply
    throw an error.
    
    With this fix, the method searches for a lot in the destination company
    that matches the one in the source company (same name and same product).
    A new lot is created by duplicating the original, if none is found.

--- purchase_sale_stock_inter_company/tests/test_inter_company_purchase_sale_stock.py
+++ purchase_sale_stock_inter_company/tests/test_inter_company_purchase_sale_stock.py
@@ -26,6 +26,8 @@ class TestPurchaseSaleStockInterCompany(TestPurchaseSaleInterCompany):
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
+        cls.lot_obj = cls.env["stock.production.lot"]
+        cls.quant_obj = cls.env["stock.quant"]
         # Configure 2 Warehouse per company
         cls.warehouse_a = cls.env["stock.warehouse"].search(
             [("company_id", "=", cls.company_a.id)]
@@ -37,6 +39,48 @@ class TestPurchaseSaleStockInterCompany(TestPurchaseSaleInterCompany):
         )
         cls.warehouse_d = cls._create_warehouse("CB-WD", cls.company_b)
         cls.company_b.warehouse_id = cls.warehouse_c
+        cls.consumable_product = cls.env["product.product"].create(
+            {
+                "name": "Consumable Product",
+                "type": "consu",
+                "categ_id": cls.env.ref("product.product_category_all").id,
+                "qty_available": 100,
+            }
+        )
+        cls.stockable_product_serial = cls.env["product.product"].create(
+            {
+                "name": "Stockable Product Tracked by Serial",
+                "type": "product",
+                "tracking": "serial",
+                "categ_id": cls.env.ref("product.product_category_all").id,
+            }
+        )
+        # Add quants for product tracked by serial to supplier
+        cls.serial_1 = cls._create_serial_and_quant(
+            cls.stockable_product_serial, "111", cls.company_b
+        )
+        cls.serial_2 = cls._create_serial_and_quant(
+            cls.stockable_product_serial, "222", cls.company_b
+        )
+        cls.serial_3 = cls._create_serial_and_quant(
+            cls.stockable_product_serial, "333", cls.company_b
+        )
+    
+    @classmethod
+    def _create_serial_and_quant(cls, product, name, company, quant=True):
+        lot = cls.lot_obj.create(
+            {"product_id": product.id, "name": name, "company_id": company.id}
+        )
+        if quant:
+            cls.quant_obj.create(
+                {
+                    "product_id": product.id,
+                    "location_id": cls.warehouse_a.lot_stock_id.id,
+                    "quantity": 1,
+                    "lot_id": lot.id,
+                }
+            )
+        return lot
 
     def test_deliver_to_warehouse_a(self):
         self.purchase_company_a.picking_type_id = self.warehouse_a.in_type_id
@@ -162,3 +206,89 @@ class TestPurchaseSaleStockInterCompany(TestPurchaseSaleInterCompany):
         # A backorder should have been made for both
         self.assertTrue(len(sale.picking_ids) > 1)
         self.assertEqual(len(purchase.picking_ids), len(sale.picking_ids))
+    
+    def test_sync_picking_lot(self):
+        """
+        Test that the lot is synchronized on the moves
+        by searching or creating a new lot in the company of destination
+        """
+        # lot 3 already exists in company_a
+        serial_3_company_a = self._create_serial_and_quant(
+            self.stockable_product_serial, "333", self.company_a
+        )
+        self.company_a.sync_picking = True
+        self.company_b.sync_picking = True
+
+        purchase = self._create_purchase_order(
+            self.partner_company_b, self.stockable_product_serial
+        )
+        sale = self._approve_po(purchase)
+
+        # validate the SO picking
+        po_picking_id = purchase.picking_ids
+        so_picking_id = sale.picking_ids
+
+        so_move = so_picking_id.move_lines
+        so_move.move_line_ids = [
+            (
+                0,
+                0,
+                {
+                    "location_id": so_move.location_id.id,
+                    "location_dest_id": so_move.location_dest_id.id,
+                    "product_id": self.stockable_product_serial.id,
+                    "product_uom_id": self.stockable_product_serial.uom_id.id,
+                    "qty_done": 1,
+                    "lot_id": self.serial_1.id,
+                    "picking_id": so_picking_id.id,
+                },
+            ),
+            (
+                0,
+                0,
+                {
+                    "location_id": so_move.location_id.id,
+                    "location_dest_id": so_move.location_dest_id.id,
+                    "product_id": self.stockable_product_serial.id,
+                    "product_uom_id": self.stockable_product_serial.uom_id.id,
+                    "qty_done": 1,
+                    "lot_id": self.serial_2.id,
+                    "picking_id": so_picking_id.id,
+                },
+            ),
+            (
+                0,
+                0,
+                {
+                    "location_id": so_move.location_id.id,
+                    "location_dest_id": so_move.location_dest_id.id,
+                    "product_id": self.stockable_product_serial.id,
+                    "product_uom_id": self.stockable_product_serial.uom_id.id,
+                    "qty_done": 1,
+                    "lot_id": self.serial_3.id,
+                    "picking_id": so_picking_id.id,
+                },
+            ),
+        ]
+        so_picking_id.button_validate()
+
+        so_lots = so_move.mapped("move_line_ids.lot_id")
+        po_lots = po_picking_id.mapped("move_lines.move_line_ids.lot_id")
+        self.assertEqual(
+            len(so_lots),
+            len(po_lots),
+            msg="There aren't the same number of lots on both moves",
+        )
+        self.assertNotEqual(
+            so_lots, po_lots, msg="The lots of the moves should be different objects"
+        )
+        self.assertEqual(
+            so_lots.mapped("name"),
+            po_lots.mapped("name"),
+            msg="The lots should have the same name in both moves",
+        )
+        self.assertIn(
+            serial_3_company_a,
+            po_lots,
+            msg="Serial 333 already existed, a new one shouldn't have been created",
+        )
