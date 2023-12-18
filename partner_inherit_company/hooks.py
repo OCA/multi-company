89f48a4b472d73ae26315e96d3904ece77543ee3
Simone Rubino <simone.rubino@aion-tech.it>
2023-12-18 16:18:21 +0100

    [ADD] partner_inherit_company

---
+++ partner_inherit_company/hooks.py
@@ -0,0 +1,10 @@
+#  Copyright 2023 Simone Rubino - Aion Tech
+#  License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+from odoo import SUPERUSER_ID, api
+
+
+def post_init_hook(cr, registry):
+    env = api.Environment(cr, SUPERUSER_ID, {})
+    for partner in env["res.partner"].search([]):
+        partner.onchange_inherit_company_ids()
---
+++ partner_inherit_company/readme/DESCRIPTION.rst
@@ -0,0 +1,3 @@
+Access partners of parent, children and siblings companies.
+
+This module applies to partners the behavior defined by the mixin "Inherit Company Abstract".
