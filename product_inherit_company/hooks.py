2fddf1b54a796e09e9153f7a98041d96b33d136d
Simone Rubino <simone.rubino@aion-tech.it>
2023-12-18 16:07:59 +0100

    [ADD] product_inherit_company

---
+++ product_inherit_company/hooks.py
@@ -0,0 +1,10 @@
+#  Copyright 2023 Simone Rubino - Aion Tech
+#  License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+from odoo import SUPERUSER_ID, api
+
+
+def post_init_hook(cr, registry):
+    env = api.Environment(cr, SUPERUSER_ID, {})
+    for product in env["product.template"].search([]):
+        product.onchange_inherit_company_ids()
