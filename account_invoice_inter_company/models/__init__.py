c3dea2d98b6f37340b50b9b1f8325263a85466fb
Jordi Ballester Alomar <jordi.ballester@forgeflow.com>
2024-01-03 09:59:03 +0100

    [IMP] account_invoice_inter_company: move method _check_intercompany_product to account.move.line

--- account_invoice_inter_company/models/__init__.py
+++ account_invoice_inter_company/models/__init__.py
@@ -1,7 +1,4 @@
-# Copyright 2013-2014 Odoo SA
-# Copyright 2015-2017 Chafique Delli <chafique.delli@akretion.com>
-# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
-
 from . import account_move
+from . import account_move_line
 from . import res_config_settings
 from . import res_company
