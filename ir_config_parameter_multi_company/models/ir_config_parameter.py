737bdc9b7ef64ad1eda9e86c485bae96699c377e
eLBati <elbaddy@gmail.com>
2024-01-15 17:40:22 +0100

    REF ir_config_parameter_multi_company with company_id field

--- ir_config_parameter_multi_company/models/ir_config_parameter.py
+++ ir_config_parameter_multi_company/models/ir_config_parameter.py
@@ -1,25 +1,48 @@
 from odoo import api, fields, models
+from odoo.osv import expression
 
 
 class IrConfigMultiCompany(models.Model):
     _inherit = "ir.config_parameter"
+    company_id = fields.Many2one("res.company", "Company")
 
-    company_value_ids = fields.One2many(
-        "ir.config_parameter_company",
-        "config_parameter_id",
-        string="Company",
-        required=False,
-    )
+    _sql_constraints = [
+        ("key_uniq", "unique (key, company_id)", "Key must be unique per company.")
+    ]
 
-    # how to test? Check here
-    # https://www.odoo.com/documentation/14.0/developer/reference/cli.html#shell
-    # https://www.odoo.com/documentation/15.0/developer/reference/backend/testing.html
+    # this is needed because get_param is usually called with sudo
+    @api.model
+    def search(self, args, offset=0, limit=None, order=None, count=False):
+        if count:
+            return super().search(args, offset, limit, order, count)
+        key_present = False
+        for item in args:
+            if len(item) == 3 and item[0] == "key":
+                key_present = True
+        if not key_present:
+            return super().search(args, offset, limit, order, count)
+        else:
+            # avoid limit to fetch all values
+            records = super().search(
+                args=args, offset=offset, limit=None, order=order, count=count
+            )
+            if len(records) > 1:
+                # key must be unique per company
+                args = expression.AND(
+                    (args, [("company_id", "=", self.env.company.id)])
+                )
+                records = super().search(
+                    args=args, offset=offset, limit=None, order=order, count=count
+                )
+                if len(records) > 1:
+                    # call with limit
+                    records = super().search(
+                        args=args, offset=offset, limit=limit, order=order, count=count
+                    )
+        return records
+
     @api.model
     def _get_param(self, key):
-        params = self.search([("key", "=", key)])
-        if params and params.company_value_ids:
-            values = params.company_value_ids
-            for value in values:
-                if value.company_id == self.env.user.company_id:
-                    return value.company_value
-        return super(IrConfigMultiCompany, self)._get_param(key)
+        # because self.env.company.id is not in cache key
+        self.clear_caches()
+        return super()._get_param(key)
--- ir_config_parameter_multi_company/tests/test_ir_config_parameter_multi_company.py
+++ ir_config_parameter_multi_company/tests/test_ir_config_parameter_multi_company.py
@@ -2,39 +2,51 @@ from odoo.tests import common
 
 
 class TestIrConfigParameterMultiCompany(common.TransactionCase):
-    def test_basic_functionality(self):
-        record = self.env["ir.config_parameter"].create(
-            {"key": "testKey", "value": "testValue"}
+    def setUp(self):
+        super(TestIrConfigParameterMultiCompany, self).setUp()
+        self.user = self.env.ref("base.user_demo")
+        self.user.groups_id += self.env.ref("base.group_system")
+
+    def test_get_params_base(self):
+
+        record = (
+            self.env["ir.config_parameter"]
+            .with_user(self.user.id)
+            .create({"key": "testKey", "value": "testValue"})
         )
         value = record.get_param("testKey")
         expected_field_value = "testValue"
         self.assertEqual(value, expected_field_value)
 
-    def test_get_params(self):
+    def test_get_params_by_company(self):
         testCompany = self.env["res.company"].create({"name": "TestCompany"})
-        record = self.env["ir.config_parameter"].create(
+        self.user.company_ids += testCompany
+        self.env["ir.config_parameter"].with_user(self.user.id).create(
+            {
+                "key": "mail.catchall.alias_test",
+                "value": "CurrentCompany",
+                "company_id": self.user.company_id.id,
+            }
+        )
+        self.env["ir.config_parameter"].create(
             {
                 "key": "mail.catchall.alias_test",
                 "value": "testValue",
-                "company_value_ids": [
-                    (
-                        0,
-                        0,
-                        {
-                            "company_id": self.env.company.id,
-                            "company_value": "CurrentCompany",
-                        },
-                    ),
-                    (
-                        0,
-                        0,
-                        {"company_id": testCompany.id, "company_value": "TestCompany"},
-                    ),
-                ],
+                "company_id": testCompany.id,
             }
         )
-        value = record.get_param("mail.catchall.alias_test")
+        value = (
+            self.env["ir.config_parameter"]
+            .with_user(self.user.id)
+            .with_company(self.user.company_id)
+            .get_param("mail.catchall.alias_test")
+        )
         self.assertEqual(value, "CurrentCompany")
-        self.env.user.company_id = testCompany.id
-        value = record.get_param("mail.catchall.alias_test")
-        self.assertEqual(value, "TestCompany")
+        self.user.company_id = testCompany.id
+        value = (
+            self.env["ir.config_parameter"]
+            .with_user(self.user.id)
+            .with_company(self.user.company_id)
+            .get_param("mail.catchall.alias_test")
+        )
+        self.assertEqual(value, "testValue")
