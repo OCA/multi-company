4d82df6eefff1997a0a9b852f372df8479649ad4
Telmo Santos <telmo.santos@camptocamp.com>
2024-02-09 09:54:28 +0100

    [ADD] sale_product_company

---
+++ sale_product_company/models/product_template.py
@@ -0,0 +1,14 @@
+# Copyright 2024 Camptocamp SA
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+from odoo import api, fields, models
+
+
+class ProductTemplate(models.Model):
+    _inherit = "product.template"
+
+    sale_ok_company_ids = fields.Many2many("res.company", string="Selling Companies")
+
+    @api.onchange("company_id")
+    def _set_sale_ok_company_ids_from_company_id(self):
+        self.sale_ok_company_ids = self.company_id
