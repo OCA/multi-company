55b5d4734c86d5f7a69c88e4e1534c8f36761822
Marina Alapont <marina.alapont@forgeflow.com>
2024-04-24 13:26:55 +0200

    [IMP] mail_multicompany: search appropriate server for the from email
    This commit improves the create() overwritte on the mail_message class so that the mail server selected is the most apropriate taking into account the from email, but considering only the mail servers of the current company.

--- mail_multicompany/models/mail_message.py
+++ mail_multicompany/models/mail_message.py
@@ -19,14 +19,25 @@ class MailMessage(models.Model):
             if not vals.get("company_id"):
                 vals["company_id"] = self.env.company.id
             if not vals.get("mail_server_id"):
-                vals["mail_server_id"] = (
-                    self.sudo()
-                    .env["ir.mail_server"]
-                    .search(
-                        [("company_id", "=", vals.get("company_id", False))],
-                        order="sequence",
-                        limit=1,
-                    )
-                    .id
+                mail_server, email_from = self._find_company_mail_server(
+                    vals.get("email_from", False), vals.get("company_id", False)
                 )
+                vals.update(
+                    {
+                        "mail_server_id": mail_server.id if mail_server else False,
+                        "email_from": email_from,
+                    }
+                )
+
         return super(MailMessage, self).create(vals_list)
+
+    def _find_company_mail_server(self, email_from, company_id):
+        # We want to find the most appropriate mail server for the given email_from
+        # but limiting it to the current active company mail servers.
+        mail_servers = (
+            self.sudo().env["ir.mail_server"].search([("company_id", "=", company_id)])
+        )
+        if not mail_servers:
+            return None, email_from
+
+        return self.env["ir.mail_server"]._find_mail_server(email_from, mail_servers)
