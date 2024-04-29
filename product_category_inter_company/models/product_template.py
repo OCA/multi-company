db9c537e19bf6c51f51de9c372fd64dcf04a1106
David García <david.garcia@Itecan.local>
2024-04-29 16:51:41 +0200

    [MIG] to 15.0

---
+++ product_category_inter_company/models/product_template.py
@@ -0,0 +1,14 @@
+# Copyright (C) 2022 Akretion (<http://www.akretion.com>).
+# @author Kévin Roche <kevin.roche@akretion.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import fields, models
+
+
+class ProductTemplate(models.Model):
+    _inherit = "product.template"
+    _check_company_auto = True
+
+    categ_id = fields.Many2one(
+        check_company=True,
+    )
---
+++ product_category_inter_company/readme/DESCRIPTION.rst
@@ -0,0 +1,1 @@
+This module allows to set product categories as company dependent.
