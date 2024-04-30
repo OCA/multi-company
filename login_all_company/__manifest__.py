3a354e252964003cbd37ae1603bb816fd70a1c6a
Adam Heinz <amh@metricwise.net>
2024-04-30 12:35:13 -0400

    [MIG] login_all_company: Migration to 15.0

--- login_all_company/__manifest__.py
+++ login_all_company/__manifest__.py
@@ -5,10 +5,14 @@
     "name": "Login All Company",
     "summary": """
         Access all your companies when you log in""",
-    "version": "14.0.1.0.1",
+    "version": "15.0.0.0.0",
     "license": "AGPL-3",
     "author": "Creu Blanca,Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/multi-company",
     "depends": ["web"],
-    "data": ["templates/assets.xml"],
+    "assets": {
+        "web.assets_backend": [
+            "login_all_company/static/src/js/*.js",
+        ],
+    },
 }
