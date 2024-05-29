249a58ba5d67cb587e1fca889f5e22ee2692c7a1
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2024-05-29 17:52:37 +0200

    [FIX] product_multi_company: Don't change model description
    The description shouldn't be overriden in any child module, as it's a traversal data that then changes all the translations.

--- product_multi_company/models/product_template.py
+++ product_multi_company/models/product_template.py
@@ -7,4 +7,3 @@ from odoo import models
 class ProductTemplate(models.Model):
     _inherit = ["multi.company.abstract", "product.template"]
     _name = "product.template"
-    _description = "Product Template (Multi-Company)"
