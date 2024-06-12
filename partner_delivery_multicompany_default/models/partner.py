6c9e661951e92ad4365a32396a43fc967d883bd7
Yannick Payot <yannick.payot@camptocamp.com>
2024-06-12 09:59:03 +0200

    [IMP] x_x_multicompany_default context flag to force on write
    Add the possibility to propagate properties on write if a context key is set.
    This can be useful in case of data import

--- partner_delivery_multicompany_default/models/partner.py
+++ partner_delivery_multicompany_default/models/partner.py
@@ -1,7 +1,7 @@
 # Copyright 2023 Moduon Team S.L.
 # Copyright 2024 Camptocamp SA
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl-3.0)
-from odoo import api, models
+from odoo import models
 
 
 class Partner(models.Model):
@@ -13,21 +13,19 @@ class Partner(models.Model):
     def propagate_multicompany_delivery_carrier(self):
         self._propagate_multicompany_field("property_delivery_carrier_id")
 
-    @api.model_create_multi
-    def create(self, vals_list):
+    def _propagate_property_fields(self):
         """Propagate delivery carrier to other companies always, on creation."""
-        res = super().create(vals_list)
-        multicompany_partners = res - res.filtered("company_id")
+        super()._propagate_property_fields()
+        multicompany_partners = self - self.filtered("company_id")
         delivery_partners = multicompany_partners.filtered(
             "property_delivery_carrier_id"
         )
         # Skip if no delivery was selected
         if not delivery_partners:
-            return res
+            return
         # Skip if user has access to only one company
         alien_user_companies = self.env.user.company_ids - self.env.company
         if not alien_user_companies:
-            return res
+            return
         # Propagate delivery carrier to other companies by default
         delivery_partners.propagate_multicompany_delivery_carrier()
-        return res
