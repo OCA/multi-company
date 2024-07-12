d58a58cece822e2ece821aa24f7da610f0d90500
Saran440 <saranl@ecosoft.co.th>
2024-07-12 14:48:17 +0700

    [MIG] product_account_multicompany_default: Migration to 15.0

--- product_account_multicompany_default/__manifest__.py
+++ product_account_multicompany_default/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "Product Account Multi-Company Default",
     "summary": "Set a default account for all companies of a product",
-    "version": "16.0.0.1.0",
+    "version": "15.0.0.1.0",
     "development_status": "Alpha",
     "category": "Accounting/Accounting",
     "website": "https://github.com/OCA/multi-company",
