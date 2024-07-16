4115969e51e1267b2301fa13b526529c618d8522
ArnauCForgeFlow <arnau.cruz@forgeflow.com>
2024-07-16 10:23:51 +0200

    [IMP] partner_multi_company: prevent inconsistencies between user and partner companies

--- partner_multi_company/models/res_users.py
+++ partner_multi_company/models/res_users.py
@@ -1,29 +1,34 @@
 # Copyright 2015-2016 Pedro M. Baeza <pedro.baeza@tecnativa.com>
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html.html
 
-from odoo import api, models
+from odoo import Command, api, models
 
 
 class ResUsers(models.Model):
     _inherit = "res.users"
 
     @api.model_create_multi
-    def create(self, vals):
-        res = super().create(vals)
-        if "company_ids" in vals:
-            res.partner_id.company_ids = vals["company_ids"]
-        if "company_id" in vals and res.partner_id.company_ids:
-            res.partner_id.company_id = vals["company_id"]
-        return res
+    def create(self, vals_list):
+        users = super().create(vals_list)
+        for user in users:
+            user.partner_id.company_ids = user.company_ids
+        return users
 
     def write(self, vals):
-        res = super().write(vals)
+        res = super(ResUsers, self.with_context(from_res_users=True)).write(vals)
         if "company_ids" in vals:
             for user in self.sudo():
-                if user.partner_id.company_ids:
-                    user.partner_id.company_ids = vals["company_ids"]
-        if "company_id" in vals:
-            for user in self.sudo():
-                if user.partner_id.company_ids:
-                    user.partner_id.company_ids = [(4, vals["company_id"])]
+                commands = []
+                company_ids_data = vals["company_ids"]
+                if isinstance(company_ids_data, list) and user.partner_id.company_ids:
+                    for item in company_ids_data:
+                        if isinstance(item, (list | tuple)):
+                            if item[0] == Command.LINK:
+                                commands.append(item)
+                            if item[0] == Command.SET:
+                                for company_id in item[2]:
+                                    commands.append(Command.link(company_id))
+                        else:
+                            commands.append(Command.link(item))
+                    user.partner_id.company_ids = commands
         return res
