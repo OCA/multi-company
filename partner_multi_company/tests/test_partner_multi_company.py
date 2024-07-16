ddc9cc742254a30c12cacf8258d3d0a856af6e40
ArnauCForgeFlow <arnau.cruz@forgeflow.com>
2024-07-16 10:23:51 +0200

    [IMP] partner_multi_company: prevent inconsistencies between user and partner companies

--- partner_multi_company/tests/test_partner_multi_company.py
+++ partner_multi_company/tests/test_partner_multi_company.py
@@ -2,8 +2,9 @@
 # Copyright 2015-2019 Pedro M. Baeza <pedro.baeza@tecnativa.com>
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
-from odoo.exceptions import AccessError
-from odoo.tests import common, tagged
+from odoo import Command
+from odoo.exceptions import AccessError, ValidationError
+from odoo.tests import Form, common, tagged
 
 
 @tagged("post_install", "-at_install")
@@ -15,73 +16,87 @@ class TestPartnerMultiCompany(common.TransactionCase):
         cls.partner_model = cls.env["res.partner"].with_context(
             mail_create_nosubscribe=True,
         )
-        cls.company_1 = cls.env["res.company"].create({"name": "Test company 1"})
-        cls.company_2 = cls.env["res.company"].create({"name": "Test company 2"})
+        cls.company_1 = cls.env["res.company"].create([{"name": "Test company 1"}])
+        cls.company_2 = cls.env["res.company"].create([{"name": "Test company 2"}])
         cls.partner_company_none = cls.partner_model.create(
-            {"name": "partner without company", "company_ids": False}
+            [{"name": "partner without company", "company_ids": False}]
         )
         cls.partner_company_1 = cls.partner_model.create(
-            {
-                "name": "partner from company 1",
-                "company_ids": [(6, 0, cls.company_1.ids)],
-            }
+            [
+                {
+                    "name": "partner from company 1",
+                    "company_ids": [Command.set(cls.company_1.ids)],
+                }
+            ]
         )
         cls.partner_company_2 = cls.partner_model.create(
-            {
-                "name": "partner from company 2",
-                "company_ids": [(6, 0, cls.company_2.ids)],
-            }
+            [
+                {
+                    "name": "partner from company 2",
+                    "company_ids": [Command.set(cls.company_2.ids)],
+                }
+            ]
         )
         cls.partner_company_both = cls.partner_model.create(
-            {
-                "name": "partner for both companies",
-                "company_ids": [(6, 0, (cls.company_1 + cls.company_2).ids)],
-            }
+            [
+                {
+                    "name": "partner for both companies",
+                    "company_ids": [Command.set((cls.company_1 + cls.company_2).ids)],
+                }
+            ]
         )
         cls.user_company_1 = cls.env["res.users"].create(
-            {
-                "name": "User company 1",
-                "login": "user_company_1",
-                "email": "somebody@somewhere.com",
-                "groups_id": [
-                    (4, cls.env.ref("base.group_partner_manager").id),
-                    (4, cls.env.ref("base.group_user").id),
-                ],
-                "company_id": cls.company_1.id,
-                "company_ids": [(6, 0, cls.company_1.ids)],
-            }
+            [
+                {
+                    "name": "User company 1",
+                    "login": "user_company_1",
+                    "email": "somebody@somewhere.com",
+                    "groups_id": [
+                        Command.link(cls.env.ref("base.group_partner_manager").id),
+                        Command.link(cls.env.ref("base.group_user").id),
+                    ],
+                    "company_id": cls.company_1.id,
+                    "company_ids": [Command.set(cls.company_1.ids)],
+                }
+            ]
         )
         cls.user_company_2 = cls.env["res.users"].create(
-            {
-                "name": "User company 2",
-                "login": "user_company_2",
-                "email": "somebody@somewhere.com",
-                "groups_id": [
-                    (4, cls.env.ref("base.group_partner_manager").id),
-                    (4, cls.env.ref("base.group_user").id),
-                ],
-                "company_id": cls.company_2.id,
-                "company_ids": [(6, 0, cls.company_2.ids)],
-            }
+            [
+                {
+                    "name": "User company 2",
+                    "login": "user_company_2",
+                    "email": "somebody@somewhere.com",
+                    "groups_id": [
+                        Command.link(cls.env.ref("base.group_partner_manager").id),
+                        Command.link(cls.env.ref("base.group_user").id),
+                    ],
+                    "company_id": cls.company_2.id,
+                    "company_ids": [Command.set(cls.company_2.ids)],
+                }
+            ]
         )
         cls.partner_company_1 = cls.partner_company_1.with_user(cls.user_company_1)
         cls.partner_company_2 = cls.partner_company_2.with_user(cls.user_company_2)
 
+    def test_users(self):
+        self.assertEqual(self.user_company_1.partner_id.company_ids, self.company_1)
+        self.assertEqual(self.user_company_2.partner_id.company_ids, self.company_2)
+
     def test_create_partner(self):
         partner = self.env["res.partner"].create(
-            {"name": "Test", "company_ids": [(4, self.env.company.id)]}
+            [{"name": "Test", "company_ids": [Command.link(self.env.company.id)]}]
         )
         company = self.env.company
         self.assertIn(company.id, partner.company_ids.ids)
         partner = self.env["res.partner"].create(
-            {"name": "Test 2", "company_ids": [(4, self.company_1.id)]}
+            [{"name": "Test 2", "company_ids": [Command.link(self.company_1.id)]}]
         )
         self.assertEqual(
             partner.with_user(self.user_company_1).company_id.id,
             self.company_1.id,
         )
         partner = self.env["res.partner"].create(
-            {"name": "Test 2", "company_ids": [(5, False)]}
+            [{"name": "Test 2", "company_ids": [Command.clear()]}]
         )
         self.assertFalse(partner.company_id)
 
@@ -102,19 +117,23 @@ class TestPartnerMultiCompany(common.TransactionCase):
 
     def test_create_company_1(self):
         partner = self.partner_model.with_user(self.user_company_1).create(
-            {
-                "name": "Test from user company 1",
-                "company_ids": [(6, 0, self.company_1.ids)],
-            }
+            [
+                {
+                    "name": "Test from user company 1",
+                    "company_ids": [Command.set(self.company_1.ids)],
+                }
+            ]
         )
         self.assertEqual(partner.company_id, self.company_1)
 
     def test_create_company_2(self):
         partner = self.partner_model.with_user(self.user_company_2).create(
-            {
-                "name": "Test from user company 2",
-                "company_ids": [(6, 0, self.company_2.ids)],
-            }
+            [
+                {
+                    "name": "Test from user company 2",
+                    "company_ids": [Command.set(self.company_2.ids)],
+                }
+            ]
         )
         self.assertEqual(partner.company_id, self.company_2)
 
@@ -148,6 +167,62 @@ class TestPartnerMultiCompany(common.TransactionCase):
             self.company_2,
         )
 
+    def test_switch_user_partner_company_form(self):
+        self.user_company_1.partner_id.company_ids = self.company_1
+        with Form(self.user_company_1, "base.view_users_form") as user:
+            user.company_ids.add(self.company_2)
+        self.assertEqual(
+            self.user_company_1.partner_id.company_ids,
+            self.company_1 | self.company_2,
+        )
+        with Form(self.user_company_1, "base.view_users_form") as user:
+            user.company_ids.remove(self.company_2.id)
+        self.assertEqual(
+            self.user_company_1.partner_id.company_ids,
+            self.company_1 | self.company_2,
+        )
+
+    def test_switch_user_partner_company_ids(self):
+        self.user_company_1.partner_id.company_ids = self.company_1
+        self.user_company_1.company_ids = (self.company_1 | self.company_2).ids
+        self.assertEqual(
+            self.user_company_1.partner_id.company_ids,
+            self.company_1 | self.company_2,
+        )
+        self.user_company_1.company_ids = self.company_1.ids
+        self.assertEqual(
+            self.user_company_1.partner_id.company_ids,
+            self.company_1 | self.company_2,
+        )
+
+    def test_switch_user_partner_company_set(self):
+        self.user_company_1.partner_id.company_ids = self.company_1
+        self.user_company_1.company_ids = [
+            Command.set([self.company_1.id, self.company_2.id])
+        ]
+        self.assertEqual(
+            self.user_company_1.partner_id.company_ids,
+            self.company_1 | self.company_2,
+        )
+        self.user_company_1.company_ids = [Command.set([self.company_1.id])]
+        self.assertEqual(
+            self.user_company_1.partner_id.company_ids,
+            self.company_1 | self.company_2,
+        )
+
+    def test_switch_user_partner_company_link(self):
+        self.user_company_1.partner_id.company_ids = self.company_1
+        self.user_company_1.company_ids = [Command.link(self.company_2.id)]
+        self.assertEqual(
+            self.user_company_1.partner_id.company_ids,
+            self.company_1 | self.company_2,
+        )
+        self.user_company_1.company_ids = [Command.unlink(self.company_2.id)]
+        self.assertEqual(
+            self.user_company_1.partner_id.company_ids,
+            self.company_1 | self.company_2,
+        )
+
     def test_commercial_fields_implementation(self):
         """It should add company_ids to commercial fields."""
         self.assertIn(
@@ -158,7 +233,7 @@ class TestPartnerMultiCompany(common.TransactionCase):
     def test_commercial_fields_result(self):
         """It should add company_ids to children partners."""
         partner = self.env["res.partner"].create(
-            {"name": "Child test", "parent_id": self.partner_company_both.id}
+            [{"name": "Child test", "parent_id": self.partner_company_both.id}]
         )
         self.assertEqual(
             partner.company_ids,
@@ -166,8 +241,17 @@ class TestPartnerMultiCompany(common.TransactionCase):
         )
 
     def test_avoid_updating_company_ids_in_global_partners(self):
-        self.user_company_1.write({"company_ids": [(4, self.company_2.id)]})
+        self.user_company_1.write({"company_ids": [Command.link(self.company_2.id)]})
         user_partner = self.user_company_1.partner_id
-        user_partner.write({"company_id": False, "company_ids": [(5, False)]})
+        user_partner.write({"company_id": False, "company_ids": [Command.clear()]})
         self.user_company_1.write({"company_id": self.company_2.id})
         self.assertEqual(user_partner.company_ids.ids, [])
+
+    def test_partner_check_company_id(self):
+        with self.assertRaisesRegex(
+            ValidationError,
+            "The partner must have at least all the companies associated with the user",
+        ):
+            self.user_company_1.partner_id.write(
+                {"company_ids": [Command.set(self.company_2.ids)]}
+            )
