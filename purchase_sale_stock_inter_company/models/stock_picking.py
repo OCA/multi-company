6b5a7c152efb0045d8edf30c99864768e101f395
Carlos Lopez <carlos.lopez@tecnativa.com>
2024-11-29 06:52:46 -0500

    [FIX] purchase_sale_stock_inter_company: Allow reception with serial tracking and partial quantity (Dropshipping)
    Issue: When handling intercompany dropshipping with serialized products, an error is raised during reception if the quantity received is less than the original purchase order's quantity. The error message states: "Mismatch between move lines with the corresponding."
    
    Steps to Reproduce:
    
    Settings:
    
    In General Settings (both companies):
    Enable Sales from Purchase Orders.
    Disable Sales Order Auto Validation.
    In Purchase Settings: Enable Dropshipping.
    In Inventory Settings: Enable Multi-Step Routes.
    Setup:
    
    In Company 1:
    Configure a product with vendor information linked to Company 2.
    In Company 2:
    Configure the same product with vendor information linked to any other vendor (not a company contact).
    Scenario:
    
    In Company 1:
    Create a sales order with a line using the Dropshipping route and the configured product.
    Confirm the sales order.
    Navigate to Purchase Orders, find the related record, and confirm it.
    Switch to Company 2:
    Locate the generated sales order and update the sales line to use the Dropshipping route.
    Set the quantity to 2 and confirm the sales order.
    Go to Purchase Orders, find the related record, and confirm it.
    Navigate to Receipts for the purchase order, enter the lot/serial numbers, and attempt to validate the receipt.
    Result: An error is raised, preventing the validation of the receipt due to a mismatch between the move lines and their corresponding records.

--- purchase_sale_stock_inter_company/models/stock_picking.py
+++ purchase_sale_stock_inter_company/models/stock_picking.py
@@ -2,8 +2,7 @@
 # Copyright 2018 Tecnativa - Pedro M. Baeza
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
-from odoo import _, api, fields, models
-from odoo.exceptions import UserError
+from odoo import api, fields, models
 
 
 class StockPicking(models.Model):
@@ -90,9 +89,12 @@ class StockPicking(models.Model):
             purchase = pick.sale_id.auto_purchase_order_id
             if not purchase:
                 continue
-            purchase.picking_ids.write({"intercompany_picking_id": pick.id})
-            if not pick.intercompany_picking_id and purchase.picking_ids[0]:
-                pick.write({"intercompany_picking_id": purchase.picking_ids[0]})
+            po_picking_pending = purchase.picking_ids.filtered(
+                lambda x: x.state not in ["done", "cancel"]
+            )
+            po_picking_pending.intercompany_picking_id = pick.id
+            if not pick.intercompany_picking_id and po_picking_pending[0]:
+                pick.intercompany_picking_id = po_picking_pending[0]
             po_picks |= pick._set_intercompany_picking_qty(purchase)
             pick._action_done_intercompany_actions(purchase)
         return super()._action_done()
@@ -102,31 +104,29 @@ class StockPicking(models.Model):
         try:
             pick = self
             for move in pick.move_lines:
-                move_lines = move.move_line_ids
-                po_move_lines = (
-                    move.sale_line_id.auto_purchase_line_id.move_ids.filtered(
-                        lambda x, ic_pick=pick.intercompany_picking_id: x.picking_id
-                        == ic_pick
-                    ).mapped("move_line_ids")
+                move_lines = move.move_line_ids.filtered(lambda x: x.qty_done > 0)
+                po_move_pending = move.sale_line_id.auto_purchase_line_id.move_ids.filtered(
+                    lambda x, ic_pick=pick.intercompany_picking_id: x.picking_id
+                    == ic_pick
+                    and x.state not in ["done", "cancel"]
                 )
-                if len(move_lines) != len(po_move_lines):
-                    note = _(
-                        "Mismatch between move lines with the "
-                        "corresponding PO %s for assigning "
-                        "quantities and lots from %s for product %s"
-                    ) % (purchase.name, pick.name, move.product_id.name)
-                    self.activity_schedule(
-                        "mail.mail_activity_data_warning",
-                        fields.Date.today(),
-                        note=note,
-                        # Try to notify someone relevant
-                        user_id=(
-                            pick.sale_id.user_id.id
-                            or pick.sale_id.team_id.user_id.id
-                            or SUPERUSER_ID,
-                        ),
-                    )
+                po_move_lines = po_move_pending.mapped("move_line_ids")
+                move_line_diff = len(move_lines) - len(po_move_lines)
+                # generate new move lines if needed
+                # example: In purchase order of C1, we have 2 move lines
+                # and in reception of C2, we have 3 move lines(with lot or serial number)
+                # then we need to create 1 more move line in purchase order of C1
+                if move_line_diff > 0:
+                    new_move_line_vals = []
+                    for _index in range(move_line_diff):
+                        vals = po_move_pending._prepare_move_line_vals()
+                        new_move_line_vals.append(vals)
+                    po_move_lines |= po_move_lines.create(new_move_line_vals)
                 # check and assign lots here
+                # if len(move_lines) != (po_move_lines)
+                # the zip will stop at the shortest list(only with qty_done > 0)
+                # list(zip([1, 2], [1, 2, 3, 4])) = [(1, 1), (2, 2)]
+                # list(zip([1, 2, 3, 4], [1, 2])) = [(1, 1), (2, 2)]
                 for ml, po_ml in zip(move_lines, po_move_lines):
                     lot_id = ml.lot_id
                     if not lot_id:
