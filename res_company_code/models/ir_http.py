fca61113be88f9443c6fc1a52dfbd1bd380da6e4
Bhavesh Heliconia <oca@heliconia.in>
2024-12-26 16:55:16 +0530

    [IMP] res_company_code: pre-commit auto fixes

--- res_company_code/models/ir_http.py
+++ res_company_code/models/ir_http.py
@@ -10,7 +10,6 @@ class Http(models.AbstractModel):
     _inherit = "ir.http"
 
     def session_info(self):
-
         result = super().session_info()
         user = request.env.user
         display_switch_company_menu = (
