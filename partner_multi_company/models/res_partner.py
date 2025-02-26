45d2dac54fa0c77882e632b87fbec402c20b7dc0
pilarvargas-tecnativa <pilar.vargas@tecnativa.com>
2025-02-26 10:35:02 +0100

    [IMP-FIX] partner_multi_company: no longer define display_name as store
    In the partner_multi_company module, the display_name field had to be
    redefined as store and index because the module installation removed
    those field properties. As of v17 the field is no longer store so it is
    not necessary to redefine it as storable. In addition, this redefinition
    causes errors when printing reports because the field is not calculated
    when accessing and therefore only shows the data stored in the database,
    which is not updated until one of the fields of the dependency changes.

--- partner_multi_company/models/res_partner.py
+++ partner_multi_company/models/res_partner.py
@@ -2,22 +2,13 @@
 # Copyright 2015-2019 Pedro M. Baeza <pedro.baeza@tecnativa.com>
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html.html
 
-from odoo import Command, _, api, fields, models
+from odoo import Command, _, api, models
 from odoo.exceptions import ValidationError
 
 
 class ResPartner(models.Model):
     _inherit = ["multi.company.abstract", "res.partner"]
     _name = "res.partner"
-
-    # This is needed because after installation this field becomes
-    # unsearchable and unsortable. Which is not explicitly changed in this
-    # module and as such can be considered an undesired yield.
-    display_name = fields.Char(
-        compute="_compute_display_name",
-        store=True,
-        index=True,
-    )
 
     @api.model_create_multi
     def create(self, vals_list):
