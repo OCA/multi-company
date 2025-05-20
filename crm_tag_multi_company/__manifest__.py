73096efe72bffb33867855e09925d045c69496f7
javierjcf <javier@comunitea.com>
2025-05-20 14:49:56 +0200

    [17.0][MIG]crm_tag_multi_company: Migration to 17.0

--- crm_tag_multi_company/__manifest__.py
+++ crm_tag_multi_company/__manifest__.py
@@ -5,7 +5,7 @@
     "name": "Crm Tag Multi Company",
     "summary": """
         This module add multi-company management to crm tag""",
-    "version": "16.0.1.0.0",
+    "version": "17.0.1.0.0",
     "license": "AGPL-3",
     "author": "ACSONE SA/NV," "Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/multi-company",
