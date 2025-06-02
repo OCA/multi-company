b07020bea45d020e6ddad4a8a4a1706442428a2c
Antonio Buric <abu@360erp.com>
2025-06-02 08:45:11 +0200

    [MIG] account_move_multi_company: Migration to 18.0

--- account_move_multi_company/__manifest__.py
+++ account_move_multi_company/__manifest__.py
@@ -5,7 +5,7 @@
 {
     "name": "Account Move Multi-Company",
     "summary": "Allow to transfer amount to other companies",
-    "version": "16.0.1.0.0",
+    "version": "18.0.1.0.0",
     "category": "Accounting & Finance",
     "author": "Open Source Integrators, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/multi-company",
