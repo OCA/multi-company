d9ad99fd2c710d6592b694ca36af06cdae77266b
Alessandro Uffreduzzi <alessandro.uffreduzzi@pytech.it>
2025-07-18 18:02:42 +0200

    [FIX] product_multi_company: variant company related to template company

--- product_multi_company/models/product.py
+++ product_multi_company/models/product.py
@@ -1,7 +1,7 @@
 # Copyright 2015-2016 Pedro M. Baeza <pedro.baeza@tecnativa.com>
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
-from odoo import api, fields, models
+from odoo import fields, models
 
 
 class ProductProduct(models.Model):
@@ -11,12 +11,5 @@ class ProductProduct(models.Model):
     company_ids = fields.Many2many(
         string="Companies",
         comodel_name="res.company",
-        relation="product_variant_companies_rel",
-        compute="_compute_company_ids",
-        store=True,
+        related="product_tmpl_id.company_ids",
     )
-
-    @api.depends("product_tmpl_id.company_ids")
-    def _compute_company_ids(self):
-        for rec in self:
-            rec.company_ids = [(6, 0, rec.product_tmpl_id.company_ids.ids)]
