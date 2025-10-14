504b97b0c20694fed53760fb5f8213109ee1c9ec
Vandan-SCS <vandan.p.serpentcs@gmail.com>
2025-10-14 19:02:16 +0530

    [FIX]partner_multi_company pre-commit.

--- partner_multi_company/__manifest__.py
+++ partner_multi_company/__manifest__.py
@@ -5,10 +5,10 @@
 {
     "name": "Partner multi-company",
     "summary": "Select individually the partner visibility on each company",
-    "version": "18.0.1.0.0",
+    "version": "19.0.1.0.0",
     "license": "AGPL-3",
     "depends": ["base_multi_company"],
-    "author": "Tecnativa, " "Odoo Community Association (OCA)",
+    "author": "Tecnativa, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/multi-company",
     "category": "Partner Management",
     "data": ["views/res_partner_view.xml"],
