608af18ed2f872c9f6e655c4ed0c0f95e8b40b4c
Emilio Pascual <emilio@moduon.team>
2026-06-19 07:45:29 +0200

    [IMP] product_multi_company: pre-commit auto fixes

--- product_multi_company/__manifest__.py
+++ product_multi_company/__manifest__.py
@@ -3,8 +3,8 @@
 
 {
     "name": "Product multi-company",
-    "summary": "Select individually the product template visibility on each " "company",
-    "author": "Tecnativa," "Odoo Community Association (OCA)",
+    "summary": "Select individually the product template visibility on each company",
+    "author": "Tecnativa,Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/multi-company",
     "category": "Product Management",
     "version": "18.0.1.0.0",
